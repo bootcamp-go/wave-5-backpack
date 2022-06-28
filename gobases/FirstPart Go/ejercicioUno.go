839a61af68ccb7de2bf1ac0f9a3e315823ce3f8d
llucumi <luz.lucumi@mercadolibre.com.co>
2022-06-28 17:10:35 -0500

    Second and First part of the class

---
+++ gobases/FirstPart Go/ejercicioUno.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	//Ejercicio nombre y dirección
+	var nombre,direccion string = "Luz Carime", "Carrera 21"
+	fmt.Println("Nombre:",nombre,"\nDirección:",direccion)
+}
