67abce864570842a899e8e0115f919fe464cd8de
FerRiosP <fernando.rios@mercadolibre.cl>
2022-06-28 17:53:32 -0400

    se mejora ejercicio

--- gobases/Bases TT/ejercicio4.go
+++ gobases/Bases TT/ejercicio4.go
@@ -1,26 +1,26 @@
-
 package main
 
 import "fmt"
 
-func main (){
+func main() {
 
 	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
-
 	fmt.Printf("La edad de Benjamin es: %v \n", employees["Benjamin"])
-	mayorDeEdad:=0
 
-	for _,age := range employees{
-		if age>21{
-			mayorDeEdad+=1;
+	mayorDeEdad := 0
+	for _, age := range employees {
+		if age > 21 {
+			mayorDeEdad += 1
 		}
 	}
-	fmt.Printf("Hay %v mayores de 21 \n",mayorDeEdad)
+	// Se imprime la cantidad empleados mayores a 21
+	fmt.Printf("Hay %v mayores de 21 \n", mayorDeEdad)
 
-	employees["Federico"]=25
+	// Se agrega Federico y se imprime empleados
+	employees["Federico"] = 25
 	fmt.Println(employees)
 
+	// Se elimina Pedro y se imprime empleados
 	delete(employees, "Pedro")
-
 	fmt.Println(employees)
 }
