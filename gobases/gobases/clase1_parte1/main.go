07a3ed06fa208e688eefc7a7ad8de39552b27cf5
nahuelmeli <nahuel.monserrat@mercadolibre.com>
2022-06-28 18:04:00 -0300

    done class 1

---
+++ gobases/gobases/clase1_parte1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// var name str
+// name = nahuel
+
+/*
+
+func main(){
+	name,lastname:= "Nahuel", "Monserrat"
+	fmt.Println(name, lastname)
+}
+
+*/
+
+func main(){
+	var (
+		temperatura = "Frio" // STR
+		humedad = 20 // INT
+		presion = 50.23 // float
+	)
+
+	fmt.Println(temperatura, humedad, presion)
+}
+
+
+/*
+var 1nombre string --> INCORRECTA var nombre string
+var apellido string 
+var int edad --> INCORRECTA var edad int
+1apellido := 6 ---> INCORRECTA apellido:= apellido
+var licencia_de_conducir = true --> INCORRECTA var licenciaDeConducir 
+var estatura de la persona int --> INCORRECTA var estaturaDeLaPersona int
+cantidadDeHijos := 2
+
+
+var apellido string = "Gomez"
+var edad int = "35" // --> sin comilla
+boolean := "false"// ---> sin comilla
+var sueldo string = 45857.90 // --> tipo float
+var nombre string = "Julián"
+
+
+
+*/
+
+
+
+
---
+++ gobases/gobases/clase1_parte2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+
+func main(){
+
+/* 	
+EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 
+
+palabra := "Academia"
+	fmt.Println(len(palabra)) 
+
+	for _, letra := range palabra {
+		fmt.Printf("La letra es %q \n"  , letra)
+	}
+	
+EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 EJERCICIO 1 
+*/
+
+
+
+
+
+/*
+EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  
+edad:= 23
+
+sueldo:= 110000
+tiempo:= 2
+
+if edad > 22{
+	if tiempo > 1 {
+		if sueldo > 100000 {
+			fmt.Println("Podes acceder al prestamo y no se te va a cobrar interes.")
+		}else {
+			fmt.Println("Podes acceder al prestamo pero se te cobra interes.")
+		}
+	}else {
+		fmt.Println("No cumplis con el tiempo suficiente")
+	}
+}else {
+	fmt.Println("No tenes la edad requerida.")
+} 
+
+EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  EJERCICIO 2  
+*/
+
+
+
+
+/* 
+EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3   
+
+var months = map[int]string{1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5:"Mayo", 6:"Junio", 7:"Julio",8:"Agosto",9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"}
+
+var mes = 1 
+
+fmt.Printf("El mes es %q", months[mes])
+
+EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 EJERCICIO 3 
+ */
+
+
+
+
+/*  
+EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 
+
+
+var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
+ var mayores = []int{}
+
+ for i, empleado := range employees {
+	if employees[i] > 21 {
+		mayores = append(mayores, empleado)
+	}
+ }
+ fmt.Println(employees["Benjamin"])
+ employees["Federico"] = 25
+ delete(employees, "Pedro")
+ fmt.Printf("Empleados mayores de 21: %v \n", len(mayores))
+
+
+ EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 EJERCICIO 4 
+ */
+
+
+ fmt.Println("Done")
+
+
+}
