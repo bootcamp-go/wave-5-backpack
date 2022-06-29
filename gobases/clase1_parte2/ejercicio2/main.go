5259f07c4f66b72c49b230bc73db09aa24eccd0e
ympintos <yvonne.pintos@mercadolibre.com>
2022-06-29 08:56:10 -0300

    modificando ejercicio 2

--- gobases/clase1_parte2/ejercicio2/main.go
+++ gobases/clase1_parte2/ejercicio2/main.go
@@ -5,24 +5,21 @@ import "fmt"
 func main() {
 	var edad, sueldo, antiguedad int
 	var empleado bool
-	var interes int = 0
 
 	edad = 25
-	sueldo = 404040
+	sueldo = 2323232323
 	antiguedad = 4
-	empleado = false
+	empleado = true
 
 	if edad > 22 && empleado == true && antiguedad > 1 {
 		fmt.Println("Se le puede otorgar el prestamo")
-		interes = 1
-		//otra forma de hacer esto podria ser poner otro if para comprobar el sueldo
+		if sueldo > 100000 {
+			fmt.Println("Y no te cobramos interes")
+		} else {
+			fmt.Println("Y te cobramos interes")
+		}
 	} else {
 		fmt.Println("Usted no cumple con las condiciones para que se le otorgue un prestamo")
 	}
 
-	if interes == 1 && sueldo > 100000 {
-		fmt.Println("Y no te cobramos interes")
-	} else if interes == 1 {
-		fmt.Println("Y te cobramos interes")
-	}
 }
