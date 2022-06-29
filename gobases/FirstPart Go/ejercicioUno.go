3d70d278ce12d71c15a4d0d55e16b8e116e696b7
llucumi <luz.lucumi@mercadolibre.com.co>
2022-06-29 10:01:20 -0500

    Delete of some folders

--- gobases/FirstPart Go/ejercicioUno.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-	//Ejercicio nombre y dirección
-	var nombre,direccion string = "Luz Carime", "Carrera 21"
-	fmt.Println("Nombre:",nombre,"\nDirección:",direccion)
-}
