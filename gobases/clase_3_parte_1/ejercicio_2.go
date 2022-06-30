e9a9fc12d9dd573fbf35b0a74ac39112d03b6885
Claudio Figueroa <claudio.figueroa@mercadolibre.cl>
2022-06-30 13:03:35 -0400

    fix totals

--- gobases/clase_3_parte_1/ejercicio_2.go
+++ gobases/clase_3_parte_1/ejercicio_2.go
@@ -42,15 +42,15 @@ func leerProductoCSV(filePath string, separador string) ([]Producto, error) {
 	propiedades := []string{}
 
 	productos := []Producto{}
-	for i, line := range csvProduct {
-		if i == 0 {
+	for line_number, line := range csvProduct {
+		if line_number == 0 {
 			propiedades = strings.Split(line, separador)
 		} else {
 			producto := Producto{}
 			valores := strings.Split(line, separador)
 			if len(valores[0]) > 0 {
-				for i, valor := range valores {
-					producto.setPropiedad(strings.Trim(propiedades[i], " "), strings.Trim(valor, " "))
+				for position, valor := range valores {
+					producto.setPropiedad(strings.Trim(propiedades[position], " "), strings.Trim(valor, " "))
 				}
 				productos = append(productos, producto)
 			}
@@ -61,11 +61,18 @@ func leerProductoCSV(filePath string, separador string) ([]Producto, error) {
 	return productos, nil
 }
 
+func (p Producto) Total() int {
+	return p.precio * p.cantidad
+}
+
 func printProducts(products []Producto) {
-	fmt.Printf("%.5s \t %10s \t %10s \n", "ID", "Precio", "Cantidad")
+	fmt.Printf("%.5s \t %10s  %10s\n", "ID", "Precio", "Cantidad")
+	suma := 0
 	for _, product := range products {
-		fmt.Printf("%.5d \t %10d \t %10d \n", product.id, product.precio, product.cantidad)
+		fmt.Printf("%.5d \t %10d  %10d \n", product.id, product.precio, product.cantidad)
+		suma += product.Total()
 	}
+	fmt.Printf("%5s \t %10d  %10s \n", "", suma, "")
 }
 
 func main() {
