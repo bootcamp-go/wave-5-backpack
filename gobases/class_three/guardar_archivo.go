18ae86ee858f876d51bdf000d04f8aa33fdddd33
anesquivel <andrea.esquivel@mercadolibre.com.mx>
2022-06-30 14:06:31 -0500

    fixing tabulacion

--- gobases/class_three/guardar_archivo.go
+++ gobases/class_three/guardar_archivo.go
@@ -9,7 +9,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 )
 
 type Producto struct {
@@ -20,46 +20,31 @@ type Producto struct {
 }
 
 func main() {
-	/*prod1 := Producto{1, 15.00, 2, 0}
-	prod2 := Producto{1, 11.40, 10, 0}
+	prod1 := Producto{1, 15.00, 2, 0}
+	prod2 := Producto{2, 11.40, 10, 0}
+	prod3 := Producto{3, 12.50, 7, 0}
 
-	dataProductos := []byte(formatData(prod1, prod2))
+	dataProductos := []byte(formatData(prod1, prod2, prod3))
 	err := os.WriteFile("./productos.csv", dataProductos, 0644)
 
-	fmt.Println(formatData(prod1, prod2))
+	//fmt.Println(formatData(prod1, prod2))
 	if err != nil {
-		fmt.Println("Error", err)
-	}*/
+		panic(err)
+	}
 
 	leerData()
 }
 
 func formatData(data ...Producto) string {
-	var dataString string = "ID , Precio , Cantidad \n"
+	var dataString string = "ID;Precio;Cantidad\n"
 	for _, element := range data {
-		dataString += fmt.Sprintf("%v , %v , %v \n", element.id, element.precio, element.cantidad)
+		dataString += fmt.Sprintf("%v;%v;%v\n", element.id, element.precio, element.cantidad)
 	}
 
 	return dataString
 }
 
 func leerData() {
-	/*read, _ := os.ReadFile("productos.csv")
-	data := fmt.Sprintf("%s", read)
-	//fmt.Println(len(data))
-
-	x := func(c rune) bool {
-		return !unicode.IsLetter(c)
-	}
-
-	y := func(c rune) bool {
-		return unicode.IsLetter(c)
-	}
-	fmt.Println(strings.FieldsFunc(data, x))
-	fmt.Println(strings.FieldsFunc(data, y))
-
-	//fmt.Printf(" %s", read)*/
-
 	file, err := os.Open("productos.csv")
 	if err != nil {
 		panic(err)
@@ -67,24 +52,31 @@ func leerData() {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	reader.Comma = ','
+	reader.Comma = ';'
 
 	data := ""
 
 	for {
 		record, e := reader.Read()
+
 		for key, e := range record {
-			fmt.Println(data)
 
 			if key == 0 {
-				data += fmt.Sprintf("%s\t", e)
-			} else {
-				if key == (len(record))-1 {
-					sFormated := e + strings.Repeat(" ", 9)
-					data += fmt.Sprintf("%s\n", sFormated)
-				} else {
-					data += fmt.Sprintf("%s", e)
+				data += fmt.Sprintf("%s\t\t", e)
 
+			} else {
+				if key == 2 {
+					number, _ := strconv.ParseFloat(e, 32)
+					if number > 0 {
+						data += fmt.Sprintf("%8.2f\n", number)
+
+					} else {
+						fmt.Println("e", e)
+						data += fmt.Sprintf("%s\n", e)
+
+					}
+				} else {
+					data += fmt.Sprintf("%s\t", e)
 				}
 			}
 		}
@@ -93,6 +85,8 @@ func leerData() {
 			fmt.Println(e)
 			break
 		}
-		//fmt.Println(record)
 	}
+
+	fmt.Println(data)
+
 }
