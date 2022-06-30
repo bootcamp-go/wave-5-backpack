1df362fc2d9a94beb24f04e852122a7f0e64b6b4
CamiloCalderonMELI <camilo.calderon@mercadolibre.com.co>
2022-06-30 16:39:53 -0500

    Clase 3 parte 2 Go bases III correccion

--- gobases/clase3_parte2/ejercicio4.go
+++ gobases/clase3_parte2/ejercicio4.go
@@ -38,12 +38,22 @@ func ordenamientoInsercion(array []int, c chan time.Duration) {
 
 func ordenamientoBurbuja(array []int, c chan time.Duration) {
 	start := time.Now()
-	for i := 0; i < len(array)-1; i++ {
-		for j := 0; j < len(array)-i-1; j++ {
-			if array[j] > array[j+1] {
-				array[j], array[j+1] = array[j+1], array[j]
+	var (
+		n      = len(array)
+		sorted = false
+	)
+	for !sorted {
+		swapped := false
+		for i := 0; i < n-1; i++ {
+			if array[i] > array[i+1] {
+				array[i+1], array[i] = array[i], array[i+1]
+				swapped = true
 			}
 		}
+		if !swapped {
+			sorted = true
+		}
+		n = n - 1
 	}
 	c <- time.Since(start)
 }
@@ -71,5 +81,4 @@ func ordenamiento100(c chan time.Duration, array []int) {
 	fmt.Printf("El ordenamiento de burbuja para %d elementos tardó: %s\n", elementos, <-c)
 	go ordenamientoSeleccion(array, c)
 	fmt.Printf("El ordenamiento de seleccion para %d elementos tardó: %s\n\n", elementos, <-c)
-
 }
