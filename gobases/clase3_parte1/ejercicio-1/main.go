c34136593c8b527b794db24a0ed175a774716c74
Juan Pablo Ortiz Rubio <juanpablo.ortiz@mercadolibre.com.co>
2022-06-30 11:53:24 -0500

    Ejercicios Clase 3 Parte 1"

---
+++ gobases/clase3_parte1/ejercicio-1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Producto struct {
+	id       int     `csv:"id"`
+	precio   float64 `csv:"precio"`
+	cantidad int     `csv:"cantidad"`
+}
+
+func guardarProductos(productos []Producto) {
+	// Crear los datos del csv
+	csvData := fmt.Sprintln("id,precio,cantidad")
+
+	for _, producto := range productos {
+		csvData += fmt.Sprintf("%d,%f,%d\n", producto.id, producto.precio, producto.cantidad)
+	}
+
+	// Escribir los datos en el archivo
+	err := os.WriteFile("productos.csv", []byte(csvData), 0644)
+
+	if err != nil {
+		fmt.Println("Error al escribir el archivo")
+	}
+}
+
+func main() {
+	productos := []Producto{
+		{100, 115000.0, 10},
+		{200, 120000.0, 20},
+		{3000, 130000.0, 30},
+		{400, 140000.0, 40},
+	}
+
+	guardarProductos(productos)
+
+}
