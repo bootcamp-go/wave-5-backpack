c196a4022cc7c21fee5cf65107dcaa97adef3680
fmonay <francisco.monay@mercadolibre.com>
2022-07-01 13:19:21 -0300

    Ejercicio1 terminado

---
+++ gobases/Ejercicios/punterosYCanalesEnGo/ejercicio1.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	Nombre   string
+	Apellido string
+	Edad     int
+	Correo   string
+	Password string
+}
+
+func (u usuario) detalleU() {
+	fmt.Printf("Nombre: %s\nApellido: %s\nEdad: %d\nCorreo: %s\nPassword: %s\n", u.Nombre, u.Apellido, u.Edad, u.Correo, u.Password)
+}
+
+func newUsuario(nombre string, apellido string, edad int, correo string, password string) usuario {
+	return usuario{
+		Nombre:   nombre,
+		Apellido: apellido,
+		Edad:     edad,
+		Correo:   correo,
+		Password: password,
+	}
+}
+
+func (u *usuario) cambiarNombre(nombre string, apellido string) {
+	u.Nombre = nombre
+	u.Apellido = apellido
+}
+
+func (u *usuario) cambiarEdad(edad int) {
+	u.Edad = edad
+}
+
+func (u *usuario) cambiarCorreo(correo string) {
+	u.Correo = correo
+}
+
+func (u *usuario) cambiarPassword(password string) {
+	u.Password = password
+}
---
+++ gobases/Ejercicios/punterosYCanalesEnGo/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	/*//1
+	usuario := newUsuario("Francisco", "Monay", 31, "francisco.monay@mercadolibre.com", "123456")
+
+	usuario.detalleU()
+
+	usuario.cambiarNombre("Pablo", "Funes")
+	usuario.cambiarEdad(20)
+	usuario.cambiarCorreo("pablofunes@gmail.com")
+	usuario.cambiarPassword("654321")
+
+	usuario.detalleU()
+	*/
+
+}
