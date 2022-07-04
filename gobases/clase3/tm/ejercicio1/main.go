cc199fcd9d7627faa48030a08bdfddb0593fe6ce
crisnLopezMeli <cristian.lopez@mercadolibre.com>
2022-07-04 14:10:40 -0300

    clase 3 tm ejercicio 1 guardar archivo

--- gobases/clase3/tm/ejercicio1/main.go
+++ gobases/clase3/tm/ejercicio1/main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-  data := fmt.Sprintf("1; 35.5; 5")
+	productos := []producto{
+		{1,10.45,3},
+		{2,15.80,5},
+		{3,20,10},
+	}
 
-  os.WriteFile("producto.csv", []byte(data), 0644)
+	escribirCSV(productos)
+}
+
+type producto struct {
+	id int
+	precio float64
+	cantidad int
+}
+
+func escribirCSV(productos []producto) {
+	data := fmt.Sprintln("ID,Precio,Cantidad")
+
+	for i, p := range productos {
+		if i == len(productos) - 1 {
+			data += fmt.Sprintf("%v,%v,%v",p.id, p.precio, p.cantidad)
+
+			err := os.WriteFile("productos.csv",[]byte(data),0644)
+			if err != nil {
+				log.Fatal("No se puedo escribir el archivo")
+			}
+		}
+
+		data += fmt.Sprintf("%v,%v,%v\n",p.id, p.precio, p.cantidad)
+	}
 }
