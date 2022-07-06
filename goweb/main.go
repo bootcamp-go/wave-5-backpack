5c720fbe8c70885c837a342e6272fc344394bda3
ksossa <kasossac@gmail.com>
2022-07-06 16:25:22 -0500

    get transactions

--- goweb/main.go
+++ goweb/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,22 +18,52 @@ type transaction struct {
 	Date      string
 }
 
-func hello(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hola",
-	})
+func GetAll(ctx *gin.Context) {
+	file, _ := os.ReadFile("transactions.json")
+	var transactions, ans []transaction
+	json.Unmarshal([]byte(file), &transactions)
+	issuer := ctx.Query("issuer")
+	date := ctx.Query("date")
+
+	filter := make([]bool, len(transactions))
+	for i, _ := range filter {
+		filter[i] = true
+	}
+	for i, transaction := range transactions {
+		if issuer != "" && issuer != transaction.Issuer {
+			filter[i] = false
+		}
+		if date != "" && date != transaction.Date {
+			filter[i] = false
+		}
+		if filter[i] {
+			ans = append(ans, transaction)
+		}
+	}
+	if len(ans) == 0 {
+		ctx.JSON(404, "Nada fue encontrado")
+		return
+	}
+	ctx.JSON(200, ans)
 }
 
-func GetAll(c *gin.Context) {
+func GetOne(ctx *gin.Context) {
 	file, _ := os.ReadFile("transactions.json")
 	var transactions []transaction
 	json.Unmarshal([]byte(file), &transactions)
-	c.JSON(200, transactions)
+
+	for _, transaction := range transactions {
+		if strconv.Itoa(transaction.Id) == ctx.Param("id") {
+			ctx.JSON(200, transaction)
+			return
+		}
+	}
+	ctx.JSON(404, "Id no encontrado")
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/hello", hello)
 	router.GET("/transactions", GetAll)
+	router.GET("/transaction/:id", GetOne)
 	router.Run()
 }
