3a1f29c4c30cfcbec04bfc8d2a9ceb713c2f0d41
anigrelliml <agustin.nigrelli@mercadolibre.com>
2022-07-06 17:46:44 -0300

    completed goweb I

--- goweb/1/tm/main.go
+++ goweb/1/tm/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,41 +22,92 @@ type Usuarios struct {
 }
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
 	// Saludo estandar
-	router.GET("/greet", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hola mundo!",
-		})
-	})
+	greet := router.Group("/greet")
+	{
+		greet.GET("", Greet)
+		greet.GET("/:nombre", GreetName)
+	}
 
-	// Saludo por nombre
-	router.GET("/greet/:nombre", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Hola %s!", c.Param("nombre")),
-		})
-	})
-
-	// Listado de usuarios
-	path := "1/tm/usuarios.json"
-
-	router.GET("/usuarios", func(c *gin.Context) {
-		jsonData, err := os.ReadFile(path)
-		if err != nil {
-			panic(err)
-		}
-
-		var u []Usuarios
-		err = json.Unmarshal(jsonData, &u)
-		if err != nil {
-			panic(err)
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"usuarios": u,
-		})
-	})
+	users := router.Group("/usuarios")
+	{
+		users.GET("", ListUsers)
+		users.GET("/:id", GetUser)
+	}
 
 	router.Run()
+}
+
+// Handlers
+func Greet(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Hola mundo!",
+	})
+}
+
+func GreetName(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("Hola %s!", ctx.Param("nombre")),
+	})
+}
+
+func ListUsers(ctx *gin.Context) {
+	activo := ctx.Query("activo")
+
+	usuarios := OpenJsonFile("1/tm/usuarios.json")
+
+	var usuarios_filtrados []Usuarios
+	if activo != "" {
+		for _, e := range usuarios {
+			if strconv.FormatBool(e.Activo) == activo {
+				usuarios_filtrados = append(usuarios_filtrados, e)
+			}
+		}
+	} else {
+		usuarios_filtrados = usuarios
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"usuarios": usuarios_filtrados,
+	})
+}
+
+func GetUser(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		panic(err)
+	}
+
+	usuarios := OpenJsonFile("1/tm/usuarios.json")
+
+	for _, v := range usuarios {
+		if v.Id == id {
+			ctx.JSON(http.StatusOK, gin.H{
+				"usuario": v,
+			})
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusNoContent, gin.H{
+		"message": fmt.Sprintf("No se ha encontrado un usuario con id %d", id),
+	})
+}
+
+func OpenJsonFile(path string) []Usuarios {
+	jsonData, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	var u []Usuarios
+	err = json.Unmarshal(jsonData, &u)
+	if err != nil {
+		panic(err)
+	}
+
+	return u
 }
