a04cccba4deb85c87cd57141acf088f9abec03d7
CristianLadinoD <cristian.ladino@mercadolibre.com.co>
2022-07-06 16:35:22 -0500

    Ejercicio Go bases Web l Tarde

--- goweb/main.go
+++ goweb/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-	Id           int     `json:"id"`
+	Id           string  `json:"id"`
 	Name         string  `json:"name"`
 	LastName     string  `json:"lastname"`
 	Email        string  `json:"email"`
@@ -20,32 +20,260 @@ type User struct {
 	CreationDate string  `json:"creation-date"`
 }
 
-func main() {
+//Este handler se encargará de responder a /.
+func HomePage(ctx *gin.Context) {
+	ctx.String(200, "¡Bienvenido a la Empresa Gophers!")
+}
+
+//EJERCICIO 2 M
+func GetName(ctx *gin.Context) {
 	name := "Cristian Ladino"
+	ctx.JSON(200, gin.H{"message": "Hola " + name})
+}
+
+//EJERCICIO 3 M
+func GetAllUsers(ctx *gin.Context) {
 	var users []User
 	jsonData, err := ioutil.ReadFile("users.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Crea un router con gin
-	router := gin.Default()
 
-	// EJERCICIO 2
-	router.GET("/hello-world", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hola " + name})
-	})
-
-	// EJERCICIO 3
 	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
 		log.Fatal(err)
 	}
 
-	router.GET("/users", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": &users})
-	})
+	ctx.JSON(200, gin.H{"message": &users})
+}
+
+// EJERCICIO 1 T
+func GetUserByName(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		if ctx.Param("name") == user.Name {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByLastName(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		if ctx.Param("lastname") == user.LastName {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByEmail(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		if ctx.Param("email") == user.Email {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByAge(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		AgeStr := strconv.Itoa(user.Age)
+		if ctx.Param("age") == AgeStr {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByCreationDate(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		if ctx.Param("creation-date") == user.CreationDate {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByActive(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		ActiveStr := strconv.FormatBool(user.Active)
+		if ctx.Param("active") == ActiveStr {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func GetUserByHeight(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		HeightStr := strconv.FormatFloat(user.Height, 'f', 0, 64)
+		if ctx.Param("height") == HeightStr {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+// EJERCICIO 2 T
+func GetUserById(ctx *gin.Context) {
+	var users []User
+	var usersFilter []User
+	jsonData, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal([]byte(jsonData), &users); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users {
+		if ctx.Param("id") == user.Id {
+			usersFilter = append(usersFilter, user)
+		}
+	}
+	if len(usersFilter) > 0 {
+		ctx.JSON(200, gin.H{"message": &usersFilter})
+	} else {
+		ctx.String(404, "Información del empleado ¡No existe!")
+	}
+}
+
+func main() {
+
+	// Crea un router con gin
+	router := gin.Default()
+
+	// EJERCICIO 1 T
+	//Cada vez que llamamos a GET y le pasamos una ruta, definimos un nuevo endpoint.
+	param := "active"
+	switch param {
+	case "id":
+		router.GET("/users/:id", GetUserById)
+	case "name":
+		router.GET("/users/:name", GetUserByName)
+	case "lastname":
+		router.GET("/users/:lastname", GetUserByLastName)
+	case "email":
+		router.GET("/users/:email", GetUserByEmail)
+	case "age":
+		router.GET("/users/:age", GetUserByAge)
+	case "height":
+		router.GET("/users/:height", GetUserByHeight)
+	case "active":
+		router.GET("/users/:active", GetUserByActive)
+	case "creation-date":
+		router.GET("/users/:creation-date", GetUserByCreationDate)
+	default:
+		router.GET("/users/:id", GetUserById)
+	}
+	router.GET("/", HomePage)
+	router.GET("/hello-world", GetName)
 
 	router.Run(":8080")
-
-	fmt.Println(users)
 
 }
