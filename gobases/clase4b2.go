79d5c2017c21ac5423f274f67c30a71ddfda3e6e
rjdelrio <raul.delrio@mercadolibre.cl>
2022-07-06 00:41:53 -0400

    clase4b_ultimoejercicio

--- gobases/clase4b2.go
+++ gobases/clase4b2.go
@@ -1,36 +1,93 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"os"
 )
 
-type prueba struct {
-	edad int
+type cliente struct {
+	legado          int
+	nombre_completo string
+	DNI             string
+	numTelefono     int
+	domicilio       string
+}
+
+var contadorId int
+
+func generadorLegado(aleatorio int) *int {
+	if aleatorio == 1 {
+		contadorId += 1
+		return &contadorId
+	}
+	return nil
+}
+func finalGeneradorLegado() int {
+	numero := generadorLegado(1)
+	if numero == nil {
+		panic("todo salio mal aborten")
+	}
+	return (*numero)
+}
+func readFile(path string) string {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("el archivo estaba corrupto")
+			return
+		}
+	}()
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+func verificadorDeNulos(c cliente) (*cliente, error) {
+	if c.DNI == "" {
+		return nil, errors.New("error en el dni nulo")
+	}
+	if c.domicilio == "" {
+		return nil, errors.New("error domicilio nulo")
+	}
+	if c.legado == 0 {
+		return nil, errors.New("error legado cero")
+	}
+	if c.nombre_completo == "" {
+		return nil, errors.New("error nombre nulo")
+	}
+	if c.numTelefono == 0 {
+		return nil, errors.New("error telfono nulo")
+	}
+	return &c, nil
 }
 
 func main() {
-	p1 := prueba{5}
-	p2 := prueba{4}
-	list_prueba := []prueba{p1, p2}
-	var puntero *prueba
-	// for i, j := range list_prueba {
-	// 	if i == 1 {
-	// 		puntero = &j
-	// 	}
-	// }
-	for i := 0; i < len(list_prueba); i++ {
-		if i == 1 {
-			puntero = &list_prueba[i]
-		}
+	fmt.Println("hoal")
+	legado := finalGeneradorLegado()
+	fmt.Println(legado)
+	textoVerificadoCliente := readFile("./clientes.csv")
+	fmt.Println(textoVerificadoCliente)
+	c := cliente{
+		legado:          legado,
+		nombre_completo: "polo",
+		DNI:             "A456633",
+		numTelefono:     98473839,
+		domicilio:       "los membrillos",
 	}
 
-	(*puntero).edad = 90
-	fmt.Println(list_prueba[1])
-	fmt.Println("inicio")
-	currentTime := time.Now()
-	fmt.Println("Current Time in String: ", currentTime.String())
-	hours, minutes, _ := time.Now().Clock()
-	fmt.Println("MM-DD-YYYY : ", fmt.Sprintf("%d:%02d", hours, minutes))
-	fmt.Println(26 % 3)
+	a, err := verificadorDeNulos(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(*a)
+	defer func() {
+		fmt.Println("Fin de la ejecución")
+		fmt.Println("Se detectaron varios errores en tiempo de ejecución")
+		fmt.Println("No han quedado archivos abiertos")
+
+	}()
+	panic("mierda")
+
 }
