eadd5afd73d37ec20ef62ad7868c76d0d73f5b5c
Claudio Figueroa <claudio.figueroa@mercadolibre.cl>
2022-07-06 17:00:28 -0400

    clase_1_parte_2 query and params

--- goweb/main.go
+++ goweb/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -25,6 +28,7 @@ func JSONTransactionToStruct(jsonBytes []byte) (*[]Transaction, error) {
 }
 
 func GetAll(ctx *gin.Context) {
+
 	transactionsBytes, err := os.ReadFile("./transactions.json")
 	if err != nil {
 		ctx.AbortWithStatusJSON(500, gin.H{
@@ -34,21 +38,79 @@ func GetAll(ctx *gin.Context) {
 	}
 
 	transactions, err := JSONTransactionToStruct(transactionsBytes)
+
 	if err != nil {
 		ctx.AbortWithStatusJSON(500, gin.H{
 			"err": "bad json file : " + err.Error(),
 		})
 		return
 	}
+
+	if currencyType := ctx.Query("currency"); currencyType != "" {
+		currencyType = strings.ToUpper(currencyType)
+		var filteredTransactions = make([]Transaction, 0)
+		for _, transaction := range *transactions {
+			if transaction.Currency == currencyType {
+				filteredTransactions = append(filteredTransactions, transaction)
+			}
+
+		}
+		transactions = &filteredTransactions
+	}
 	ctx.JSON(200, gin.H{
 		"data": transactions,
+	})
+}
+
+func GetOne(ctx *gin.Context) {
+
+	transactionsBytes, err := os.ReadFile("./transactions.json")
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+
+	transactions, err := JSONTransactionToStruct(transactionsBytes)
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, gin.H{
+			"err": "bad json file : " + err.Error(),
+		})
+		return
+	}
+	searchId, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"err": "param id must be integer",
+		})
+		return
+	}
+	var transactionResult *Transaction
+	for _, transaction := range *transactions {
+		if transaction.Id == searchId {
+			transactionResult = &transaction
+			break
+		}
+	}
+	if transactionResult == nil {
+		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"err": "transaction not found",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": transactionResult,
 	})
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/transactions", GetAll)
+	transactionsRoute := router.Group("/transactions")
+	transactionsRoute.GET("/", GetAll)
+	transactionsRoute.GET("/:id", GetOne)
 
 	router.Run()
 }
