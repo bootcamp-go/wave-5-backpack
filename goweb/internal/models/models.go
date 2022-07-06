1511befa5543dd423695c51ea44e630677848d96
crisnLopezMeli <cristian.lopez@mercadolibre.com>
2022-07-06 17:28:22 -0300

    clase 1 go web. Listar Entidad

---
+++ goweb/internal/models/models.go
@@ -0,0 +1,11 @@
+package models
+
+type Transaction struct {
+  ID int64 `json:"id"`
+  Cod string `json:"cod_transaction"`
+  Moneda string `json:"moneda"`
+  Monto float64 `json:"monto"`
+  Emisor string `json:"emisor"`
+  Receptor string `json:"receptor"`
+  Fecha string `json:"date_transaction"`
+}
--- goweb/main.go
+++ goweb/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/bootcamp-go/wave-5-backpack/tree/lopez_cristian/goweb/internal/models"
 	"github.com/gin-gonic/gin"
 )
+
+func GetAll(ctx *gin.Context) {	
+  jsonData, err := os.ReadFile("./transactions.json")
+  if err != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{
+      "error": "no se pudo leer el archivo",
+    })
+    return
+  }
+
+  // type donde guardamos los datos
+  var t []models.Transaction
+
+  if err := json.Unmarshal(jsonData, &t); err != nil {
+  	log.Printf("error marshalling: %v\n", err)
+  	ctx.JSON(http.StatusInternalServerError, gin.H{
+  		"error" : err,
+  	})
+  	return
+  }
+
+  log.Println(t)
+
+  // Response
+  ctx.JSON(http.StatusOK, t)
+}
 
 
 func main() {
@@ -18,6 +48,8 @@ func main() {
 			"message": data,
 		})
 	})
+
+	router.GET("/transactions", GetAll)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Println("error en el server")
