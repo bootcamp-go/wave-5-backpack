21cfc8eb1da9dd961c5d61ef5fd1ded1708a7d0c
cristobalmonsalve <cristobal.monsalve@mercadolibre.cl>
2022-07-06 17:00:57 -0400

    go web ejercicios parte 2

--- goweb/go-web/main.go
+++ goweb/go-web/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,23 +19,166 @@ type Transaction struct {
 	Fecha    string `json:"fecha"`
 }
 
-func getAll(c *gin.Context) {
+func getAll() ([]Transaction, error) {
 	var transactions []Transaction
 	file, err := os.ReadFile("./transactions.json")
+	if err != nil {
+		return transactions, err
+	}
+	if err2 := json.Unmarshal(file, &transactions); err != nil {
+		return transactions, err2
+	}
+	return transactions, nil
+}
+
+func filterById(id string) (Transaction, error) {
+	transactions, err := getAll()
+	if err != nil {
+		return Transaction{}, err
+	}
+	for _, t := range transactions {
+		if t.Id == id {
+			return t, nil
+		}
+	}
+	return Transaction{}, errors.New("No se encontro el registro")
+}
+
+func filterListById(transacciones []Transaction, id string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Id == id {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByCode(transacciones []Transaction, code string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Code == code {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByMoneda(transacciones []Transaction, moneda string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Moneda == moneda {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByMonto(transacciones []Transaction, monto int) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Monto == monto {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByEmisor(transacciones []Transaction, emisor string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Emisor == emisor {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByReceptor(transacciones []Transaction, receptor string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Receptor == receptor {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func filterListByFecha(transacciones []Transaction, fecha string) []Transaction {
+	var transaccionesFiltradas []Transaction
+	for _, t := range transacciones {
+		if t.Fecha == fecha {
+			transaccionesFiltradas = append(transaccionesFiltradas, t)
+		}
+	}
+	return transaccionesFiltradas
+}
+
+func getAllHandler(c *gin.Context) {
+	transacciones, err := getAll()
 	if err != nil {
 		c.JSON(500, gin.H{
 			"error": err.Error(),
 		})
-	}
-	err2 := json.Unmarshal(file, &transactions)
-	if err2 != nil {
-		c.JSON(500, gin.H{
-			"error": err2.Error(),
+	} else {
+		c.JSON(200, gin.H{
+			"transacciones": transacciones,
 		})
 	}
-	c.JSON(200, gin.H{
-		"transacciones": transactions,
-	})
+}
+
+func filterByFieldsHandler(c *gin.Context) {
+	transacciones, err := getAll()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		id := c.Query("id")
+		if id != "" {
+			transacciones = filterListById(transacciones, id)
+		}
+		code := c.Query("code")
+		if code != "" {
+			transacciones = filterListByCode(transacciones, code)
+		}
+		moneda := c.Query("moneda")
+		if moneda != "" {
+			transacciones = filterListByMoneda(transacciones, moneda)
+		}
+		monto := c.Query("monto")
+		if monto != "" {
+			m, _ := strconv.Atoi(monto)
+			transacciones = filterListByMonto(transacciones, m)
+		}
+		emisor := c.Query("emisor")
+		if emisor != "" {
+			transacciones = filterListByEmisor(transacciones, emisor)
+		}
+		receptor := c.Query("receptor")
+		if receptor != "" {
+			transacciones = filterListByReceptor(transacciones, receptor)
+		}
+		fecha := c.Query("fecha")
+		if fecha != "" {
+			transacciones = filterListByFecha(transacciones, fecha)
+		}
+		c.JSON(200, gin.H{
+			"transacciones": transacciones,
+		})
+	}
+}
+
+func filterByIdHandler(c *gin.Context) {
+	id := c.Param("id")
+	transaction, err := filterById(id)
+	if err != nil {
+		c.String(404, err.Error())
+	} else {
+		c.JSON(200, gin.H{
+			"transaccion": transaction,
+		})
+	}
 }
 
 func main() {
@@ -45,7 +190,13 @@ func main() {
 		})
 	})
 
-	router.GET("/transacciones", getAll)
+	router.GET("/transacciones", getAllHandler)
+
+	transacciones := router.Group("/transaccion")
+	{
+		transacciones.GET("/", filterByFieldsHandler)
+		transacciones.GET("/:id", filterByIdHandler)
+	}
 
 	router.Run()
 }
