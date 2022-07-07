90494fb4f160a73b3f0157cd4d3e43a831baf7c2
asanchez497 <andres.sanchez@mercadolibre.com>
2022-07-07 13:43:14 -0300

    goweb clase 2 matutino

--- goweb/main.go
+++ goweb/main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type usuarios struct {
 	Id            int     `json:"id"`
-	Nombre        string  `json:"nombre"`
-	Apellido      string  `json:"apellido"`
-	Email         string  `json:"email"`
-	Edad          int     `json:"edad"`
-	Altura        float64 `json:"altura"`
-	Activo        bool    `json:"activo"`
-	FechaCreacion string  `json:"fecha_de_creacion"`
+	Nombre        string  `json:"nombre" binding:"required"`
+	Apellido      string  `json:"apellido" binding:"required"`
+	Email         string  `json:"email" binding:"required"`
+	Edad          int     `json:"edad" binding:"required"`
+	Altura        float64 `json:"altura" binding:"required"`
+	Activo        bool    `json:"activo" binding:"required"`
+	FechaCreacion string  `json:"fecha_de_creacion" binding:"required"`
 }
 
 var users []usuarios = []usuarios{
 	{Id: 1, Nombre: "ABC", Apellido: "ABC", Email: "a@a.com", Edad: 21, Altura: 1.82, Activo: true, FechaCreacion: "2022-02-20"},
-	{Id: 4, Nombre: "BCD", Apellido: "BCD", Email: "b@b.com", Edad: 30, Altura: 1.76, Activo: false, FechaCreacion: "2022-02-25"},
+	{Id: 2, Nombre: "BCD", Apellido: "BCD", Email: "b@b.com", Edad: 30, Altura: 1.76, Activo: false, FechaCreacion: "2022-02-25"},
 	{},
 }
 
@@ -42,6 +44,7 @@ func GetById(ctx *gin.Context) {
 }
 
 func FilterByName(ctx *gin.Context) {
+	// ctx.Request.URL.Query()
 	name := ctx.Query("name")
 	result := []usuarios{}
 	for i := 0; i < len(users); i++ {
@@ -50,6 +53,35 @@ func FilterByName(ctx *gin.Context) {
 		}
 	}
 	ctx.JSON(200, result)
+}
+
+func Guardar() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		expectedToken := "abcd"
+		if ctx.GetHeader("token") != expectedToken {
+			ctx.JSON(401, "no tiene permisos para realizar la operacion solicitada")
+			return
+		}
+		var user usuarios
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			var ve validator.ValidationErrors
+			if errors.As(err, &ve) {
+				result := ""
+				for i, field := range ve {
+					if i != len(ve)-1 {
+						result += fmt.Sprintf("el campo %s es requerido y ", field.Field())
+					} else {
+						result += fmt.Sprintf("el campo %s es requerido", field.Field())
+					}
+				}
+				ctx.JSON(404, result)
+			}
+			return
+		}
+		user.Id = users[len(users)-1].Id + 1
+		users = append(users, user)
+		ctx.JSON(200, user)
+	}
 }
 
 func main() {
@@ -75,5 +107,6 @@ func main() {
 	router := gin.Default()
 	router.GET("usuarios/:id", GetById)
 	router.GET("usuarios", FilterByName)
+	router.POST("usuarios", Guardar())
 	router.Run()
 }
