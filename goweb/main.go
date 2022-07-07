65def7ef9f093e1bf9ee70d39d1d3c1af0e717ca
Jose González <jose.gonzalezperez@mercadolibre.com>
2022-07-07 11:43:04 -0300

    Clase1TT

--- goweb/main.go
+++ goweb/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -35,5 +37,49 @@ func main() {
 			"usuario": u,
 		})
 	})
+
+	router.GET("/getuser/:id", getOneUser)
+	router.GET("/filtrarusuarios", filterUsers)
+
 	router.Run()
+}
+
+func getOneUser(ctex *gin.Context) {
+	var u []Usuarios
+	var u2 Usuarios
+	j, _ := os.ReadFile("./usuarios.json")
+	if err := json.Unmarshal(j, &u); err != nil {
+		log.Println(string(j))
+		log.Fatal(err)
+	}
+	id, _ := strconv.Atoi(ctex.Param("id"))
+	for _, value := range u {
+		if value.Id == id {
+			u2 = value
+			break
+		}
+	}
+	ctex.JSON(200, gin.H{
+		"usuario": u2,
+	})
+}
+
+func filterUsers(ctx *gin.Context) {
+	var u []Usuarios
+	j, _ := os.ReadFile("./usuarios.json")
+	if err := json.Unmarshal(j, &u); err != nil {
+		log.Println(string(j))
+		log.Fatal(err)
+	}
+	var u2 Usuarios
+	ctx.ShouldBindQuery(&u2)
+
+	var filtrado []Usuarios
+	for _, t := range u {
+		if u2.Id == t.Id && u2.Nombre == t.Nombre && u2.Apellido == t.Apellido && u2.Email == t.Email && u2.Edad == t.Edad && u2.Altura == t.Altura && u2.Activo == t.Activo && u2.Fecha_creacion == t.Fecha_creacion {
+			filtrado = append(filtrado, t)
+		}
+	}
+
+	ctx.JSON(http.StatusOK, filtrado)
 }
