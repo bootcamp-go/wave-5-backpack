799f0cb718dd1c66d8059857faa4bc728b7a008d
dipalacios <diego.palacios@mercadolibre.com.co>
2022-07-07 10:34:57 -0500

    update C1 web

--- goweb/main.go
+++ goweb/main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type usuario struct {
-	id       int
-	nombre   string
-	apellido string
-	email    string
-	altura   float64
-	activo   bool
-	fecha    string
+type Usuario struct {
+	Id       int
+	Nombre   string
+	Apellido string
+	Email    string
+	Altura   float64
+	Activo   bool
+	Fecha    string
+}
+
+var Usuarios []Usuario
+
+func PaginaPrincipal(ctx *gin.Context) {
+	ctx.String(200, "Hola Diego")
+}
+
+func GetAll(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": Usuarios,
+	})
 }
 
 func main() {
 
-	var usuarios []usuario
-	data, err := ioutil.ReadFile("usuarios.json")
-	json.Unmarshal([]byte(data), &usuarios)
-	if err != nil {
-		fmt.Println(err)
-	}
+	Usuarios = append(Usuarios, Usuario{1, "Diego", "Palacios", "d@example.com", 1.68, true, "21/06/2022"})
+	Usuarios = append(Usuarios, Usuario{2, "Fernndo", "Palacios", "f@example.com", 1.70, true, "21/06/2022"})
+	Usuarios = append(Usuarios, Usuario{3, "Cesar", "Parrado", "c@example.com", 1.80, true, "21/06/2022"})
 
 	r := gin.Default()
-	r.GET("", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hola Diego",
-		})
-	})
-	r.GET("/usuarios", func(GetAll *gin.Context) {
-		GetAll.JSON(http.StatusOK, &usuarios)
-	})
+	r.GET("", PaginaPrincipal)
+	r.GET("/usuarios", GetAll)
 	r.Run()
 
 }
