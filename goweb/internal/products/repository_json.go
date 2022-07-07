ccd5588e830fa2ae131a571d61ffd98f3bdaccae
Juan Pablo Ortiz Rubio <juanpablo.ortiz@mercadolibre.com.co>
2022-07-07 16:11:59 -0500

    Clase 2 GoWeb Parte 2 - Ejercicios arreglado

---
+++ goweb/internal/products/repository_json.go
@@ -0,0 +1,27 @@
+package products
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
+var PATH string = "./resources/products.json"
+
+func ReadAllProducts() ([]models.Product, error) {
+	data, err := os.ReadFile(PATH)
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("error al leer el archivo products.json")
+	}
+
+	productos := []models.Product{}
+
+	if err := json.Unmarshal([]byte(data), &productos); err != nil {
+		log.Println(err)
+		return nil, errors.New("error al deserializar el archivo products.json")
+	}
+
+	return productos, nil
+}
