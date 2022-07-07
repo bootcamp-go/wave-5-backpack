81e304d8b4b4b19ff90ec84fb549cbdb9d56ebdf
Matias Vince <matias.vince@mercadolibre.com>
2022-07-07 09:28:33 -0300

    Agrego ejercicio C1 - GO Web - TM

---
+++ goweb/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Users struct {
+	Usuarios []Usuario `json:"usuarios"`
+}
+
+type Usuario struct {
+	ID            int    `json:"id"`
+	Nombre        string `json:"nombre"`
+	Apellido      string `json:"apellido"`
+	Email         string `json:"email"`
+	Edad          int    `json:"edad"`
+	Altura        int    `json:"altura"`
+	Activo        bool   `json:"activo"`
+	FechaCreacion string `json:"fecha_creacion"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/usuarios", func(c *gin.Context) {
+		var u Users
+		jsonData, _ := os.ReadFile("usuarios.json")
+
+		// fmt.Println(jsonData)
+
+		if err := json.Unmarshal([]byte(jsonData), &u); err != nil {
+			log.Fatal(err)
+		}
+
+		// fmt.Println(u)
+
+		c.JSON(200, gin.H{
+			"message": u.Usuarios,
+		})
+	})
+
+	router.Run()
+}
+
+func GetAll(c *gin.Context) {
+	var u Users
+	jsonData, _ := os.ReadFile("usuarios.json")
+
+	fmt.Println(jsonData)
+
+	if err := json.Unmarshal([]byte(jsonData), &u); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(u)
+
+	c.JSON(200, gin.H{
+		"message": u.Usuarios[0].Apellido,
+	})
+}
