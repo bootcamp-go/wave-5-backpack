03e765549d19749b575d13be85d6785053ad00ac
mcarrascodev <matias.carrasco@mercadolibre.cl>
2022-07-08 15:01:50 -0400

    Subiendo Go Bases clase1 AM

---
+++ gobases/practica_c1_am/ej1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre string = "Matias Carrasco Mella"
+	direccion := "Rey Felipe II 024, Estacion Central"
+
+	fmt.Printf("Mi nombre es: %s\n", nombre)
+	fmt.Printf("Mi dirección es: %s\n", direccion)
+}
