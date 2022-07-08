4d4aa183c2da2e0ea1014a3048240acae799ae02
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-08 13:39:07 -0300

    Se eliminan etiquetas Json de la estructura usuario en domain

--- goweb/internal/domain/usuarios.go
+++ goweb/internal/domain/usuarios.go
@@ -1,13 +1,12 @@
 package domain
 
 type Usuarios struct {
-	//el guion lo ignora
-	Id            int     `json:"-"`
-	Nombre        string  `json:"nombre" binding:"required"`
-	Apellido      string  `json:"apellido" binding:"required"`
-	Email         string  `json:"email" binding:"required"`
-	Edad          int     `json:"edad" binding:"required"`
-	Altura        float64 `json:"altura" binding:"required"`
-	Activo        bool    `json:"activo" binding:"required"`
-	FechaCreacion string  `json:"fecha_de_creacion" binding:"required"`
+	Id            int
+	Nombre        string
+	Apellido      string
+	Email         string
+	Edad          int
+	Altura        float64
+	Activo        bool
+	FechaCreacion string
 }
