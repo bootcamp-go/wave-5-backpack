5b499dc522a8c202dd155209e820721dbbb9b842
fvisintini-meli <franco.visintini@mercadolibre.com>
2022-07-11 13:10:03 -0300

    clase 4 parte 1

--- goweb/cmd/server/handler/users.go
+++ goweb/cmd/server/handler/users.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"goweb/internal/users"
+	"goweb/pkg/web"
 	"github.com/gin-gonic/gin"
 	"strconv"
 	"fmt"
 	"os"
 	"net/http"
+	"strings"
 )
 
 type request struct {
@@ -35,17 +37,15 @@ func (c *User) GetAllUsers() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 		u, err := c.service.GetAllUsers()
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, u)
+		ctx.JSON(200, web.NewResponse(200, u, ""))
 	}
 }
 
@@ -54,18 +54,16 @@ func (c *User) GetUserById() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 		id,_ := strconv.Atoi(ctx.Param("id"))
 		u, err := c.service.GetUserById(id)
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, u)
+		ctx.JSON(200, web.NewResponse(200, u, ""))
 	}
 }
 
@@ -75,27 +73,29 @@ func (c *User) StoreUser() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 
 		// traigo los datos del post y los guardo en una variable del tipo struct request que generé arriba
 		var req request
 		if err := ctx.Bind(&req); err !=nil{
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
+			return
+		}
+
+		// valido campos requeridos
+		if v := validar(req); v != "" {
+			ctx.JSON(400, web.NewResponse(400, nil, v))
 			return
 		}
 
 		newUser, err := c.service.StoreUser(req.Name, req.LastName, req.Email, req.Age, req.Height, req.Active, req.CreatedAt)
 		if err != nil{
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, newUser)
+		ctx.JSON(200, web.NewResponse(200, newUser, ""))
 	}
 }
 
@@ -104,55 +104,37 @@ func (c *User) UpdateTotal() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 		
-		var errores []string
+		//var errores []string
 		
 		id,err := strconv.Atoi(ctx.Param("id"))
 		if err != nil {
-			ctx.JSON(400, gin.H{ "error":  "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
  			return
 		}
 
 		// validaciones
 		var req request
 		if err:= ctx.ShouldBindJSON(&req); err!=nil{
-			ctx.JSON(40, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		if req.Name == ""{
-			errores = append(errores, "El nombre del usuario es requerido")
-		}
-		if req.LastName == ""{
-			errores = append(errores, "El apellido del usuario es requerido")
-		}
-		if req.Email == ""{
-			errores = append(errores, "El email del usuario es requerido")
-		}
-		if req.Age == 0 {
-			errores = append(errores, "La edad del usuario es requerido")
-		}
-		if req.Height == 0 {
-			errores = append(errores, "La altura del usuario es requerido")
-		}
-		if req.CreatedAt == ""{
-			errores = append(errores, "La fecha de creacion del usuario es requerido")
-		}
-		if len(errores) > 0 {
-			ctx.JSON(400, gin.H{"errores": errores})
+		// valido campos requeridos
+		if v := validar(req); v != "" {
+			ctx.JSON(400, web.NewResponse(400, nil, v))
 			return
 		}
-
 
 		userUpdated, err:=	 c.service.UpdateTotal(id, req.Name, req.LastName, req.Email, req.Age, req.Height, req.Active, req.CreatedAt)
 
 		if err !=nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, userUpdated)
+		ctx.JSON(200, web.NewResponse(200, userUpdated, ""))
 	}
 }
 
@@ -161,7 +143,7 @@ func (c *User) UpdatePartial() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 
@@ -169,34 +151,35 @@ func (c *User) UpdatePartial() gin.HandlerFunc {
 
 		id,err := strconv.Atoi(ctx.Param("id"))
 		if err != nil {
-			ctx.JSON(400, gin.H{ "error":  "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
  			return
 		}
 
 		// validaciones
 		var req request
 		if err:= ctx.ShouldBindJSON(&req); err!=nil{
-			ctx.JSON(40, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
 		if req.LastName == ""{
 			errores = append(errores, "El apellido del usuario es requerido")
 		}
 		if req.Age == 0 {
-			errores = append(errores, "La edad del usuario es requerido")
+			errores  =append(errores, "La edad del usuario es requerido")
 		}
 		if len(errores) > 0 {
-			ctx.JSON(400, gin.H{"errores": errores})
+			erroresStr := strings.Join(errores, ", ")
+			ctx.JSON(400, web.NewResponse(400, nil, erroresStr))
 			return
 		}
 
 		userUpdated, err:=	 c.service.UpdatePartial(id, req.LastName, req.Age)
 
 		if err !=nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, userUpdated)
+		ctx.JSON(200, web.NewResponse(200, userUpdated, ""))
 	}
 }
 
@@ -205,22 +188,49 @@ func (c *User) Delete() gin.HandlerFunc {
 		// valido token
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, "Token inválido"))
 			return
 		}
 		
 		id,err := strconv.Atoi(ctx.Param("id"))
 		if err != nil {
-			ctx.JSON(400, gin.H{ "error":  "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
  			return
 		}
 
 		err=c.service.Delete(id)
 		if err !=nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, gin.H{ "data": fmt.Sprintf("El producto %d ha sido eliminado",
-		id) })
+		ctx.JSON(200, web.NewResponse(200, fmt.Sprintf("El producto %d ha sido eliminado",
+		id), ""))
 	}
+}
+
+func validar(req request) string {
+	var response []string
+	if req.Name == ""{
+		response = append(response, "Nombre") 
+	}
+	if req.LastName == ""{
+		response = append(response, "Apellido")
+	}
+	if req.Email == ""{
+		response = append(response, "Email")
+	}
+	if req.Age == 0 {
+		response = append(response, "Edad")
+	}
+	if req.Height == 0 {
+		response = append(response,"Altura")
+	}
+	if req.CreatedAt == ""{
+		response = append(response, "Fecha de creación")
+	}
+	if len(response) > 0 {
+		errores := "Por favor completar los siguientes campos: "+strings.Join(response, ", ")
+		return errores
+	}
+	return ""
 }
