a0e13c3fe88195c79224a5aba65d92ecf559e89a
anigrelliml <agustin.nigrelli@mercadolibre.com>
2022-07-11 12:53:15 -0300

    refactor code type in response

--- goweb/4/tm/pkg/web/response.go
+++ goweb/4/tm/pkg/web/response.go
@@ -1,16 +1,14 @@
 package web
 
-import "strconv"
-
 type Response struct {
-	Code  string      `json:"code"`
+	Code  int         `json:"code"`
 	Data  interface{} `json:"data,omitempty"`
 	Error string      `json:"error,omitempty"`
 }
 
 func NewResponse(code int, data interface{}, err string) Response {
 	return Response{
-		Code:  strconv.FormatInt(int64(code), 10),
+		Code:  code,
 		Data:  data,
 		Error: err,
 	}
