ecfaf7948ab93e17178df6487c3a661c57a408c6
SimVarg <simon.varela@mercadolibre.com>
2022-07-11 12:06:32 -0300

    ejercicios clase 1 parte 1

---
+++ goweb/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactions struct {
+	Id       string
+	Codigo   string
+	Moneda   string
+	Monto    float64
+	Emisor   string
+	Receptor string
+	Fecha    string
+}
+
+var t []transactions
+
+func main() {
+
+	file, err := os.ReadFile("./transactions.json")
+	if err != nil {
+		panic(err)
+	}
+
+	if e := json.Unmarshal(file, &t); e != nil {
+		log.Fatal(e)
+	}
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hola Simón",
+		})
+	})
+
+	router.GET("/transactions", GetAll)
+}
+
+func GetAll(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, t)
+}
