28837617190fb3c0b6cf1ce833e2ff2225c370b0
dipalacios <diego.palacios@mercadolibre.com.co>
2022-07-12 09:31:39 -0500

    add docs

--- goweb-capas/cmd/server/handler/products.go
+++ goweb-capas/cmd/server/handler/products.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"fmt"
 	"goweb-capas/internal/products"
+	"goweb-capas/pkg/web"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -26,160 +28,202 @@ func NewProduct(p products.Service) *Product {
 	}
 }
 
+// ListProducts godoc
+// @Summary List products
+// @Tags Products
+// @Description get products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /products [get]
 func (c *Product) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.Request.Header.Get("token")
-		if token != "123456" {
-			ctx.JSON(401, gin.H{
-				"error": "token inválido",
-			})
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(401, web.NewResponse(401, nil, "Error: Token invalido"))
 			return
 		}
 
 		p, err := c.service.GetAll()
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, p)
+		ctx.JSON(200, web.NewResponse(200, p, ""))
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [post]
 func (c *Product) Store() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.Request.Header.Get("token")
-		if token != "123456" {
-			ctx.JSON(401, gin.H{"error": "token inválido"})
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(401, web.NewResponse(401, nil, "Error: Token invalido"))
 			return
 		}
 		var req request
 		if err := ctx.Bind(&req); err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
 		p, err := c.service.Store(req.Name, req.Type, req.Count, req.Price)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, p)
+		ctx.JSON(200, web.NewResponse(200, p, ""))
 	}
 }
 
+// StoreProducts godoc
+// @Summary Update products
+// @Tags Products
+// @Description Update products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [put]
 func (p *Product) Update() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
-		if token != "123456" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+		if token != os.Getenv("TOKEN") {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(401, nil, "Error: Token invalido"))
 			return
 		}
 
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(400, gin.H{"error": "Id inválido"})
+			c.JSON(400, web.NewResponse(400, nil, "Error: Id inválido"))
 			return
 		}
 
 		var req request
 		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
+			c.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
 
 		if req.Name == "" {
-			c.JSON(400, gin.H{"error": "El nombre del producto es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "El nombre del producto es requerido"))
 			return
 		}
 
 		if req.Type == "" {
-			c.JSON(400, gin.H{"error": "El tipo de producto es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "El tipo de producto es requerido"))
 			return
 		}
 
 		if req.Count == 0 {
-			c.JSON(400, gin.H{"error": "La cantidad es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "La cantidad es requerido"))
 			return
 		}
 
 		if req.Price == 0 {
-			c.JSON(400, gin.H{"error": "El precio es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "El precio es requerido"))
 			return
 		}
 
 		p, err := p.service.Update(int(id), req.Name, req.Type, req.Count, req.Price)
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
 
-		c.JSON(200, p)
+		c.JSON(200, web.NewResponse(200, p, ""))
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param product body request true "Product to store"
+// @Success 200 {object} web.Response
+// @Router /products [patch]
 func (p *Product) Patch() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
-		if token != "123456" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+		if token != os.Getenv("TOKEN") {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(401, nil, "Error: Token invalido"))
 			return
 		}
 
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(400, gin.H{"error": "Id inválido"})
+			c.JSON(400, web.NewResponse(400, nil, "Error: Id inválido"))
 			return
 		}
 
 		var req request
 		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
+			c.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
 
 		if req.Name == "" {
-			c.JSON(400, gin.H{"error": "El nombre del producto es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "El nombre del producto es requerido"))
 			return
 		}
 
 		if req.Price == 0 {
-			c.JSON(400, gin.H{"error": "El precio es requerido"})
+			c.JSON(400, web.NewResponse(400, nil, "El precio es requerido"))
 			return
 		}
 
 		p, err := p.service.Patch(int(id), req.Name, req.Price)
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
 
-		c.JSON(200, p)
+		c.JSON(200, web.NewResponse(200, p, ""))
 	}
 }
 
+// StoreProducts godoc
+// @Summary Store products
+// @Tags Products
+// @Description store products
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /products [delete]
 func (p *Product) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
-		if token != "123456" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+		if token != os.Getenv("TOKEN") {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(401, nil, "Error: Token invalido"))
 			return
 		}
 
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(400, gin.H{"error": "Id inválido"})
+			c.JSON(400, web.NewResponse(400, nil, "Error: Id inválido"))
 			return
 		}
 
 		err = p.service.Delete(int(id))
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
 
-		c.JSON(200, gin.H{"data": fmt.Sprintf("El producto %d ha sido eliminado", id)})
+		c.JSON(200, web.NewResponse(200, fmt.Sprintf("El producto %d ha sido eliminado", id), ""))
 	}
 }
--- goweb-capas/internal/products/repository.go
+++ goweb-capas/internal/products/repository.go
@@ -1,6 +1,9 @@
 package products
 
-import "fmt"
+import (
+	"fmt"
+	"goweb-capas/pkg/store"
+)
 
 type Product struct {
 	ID    int     `json:"id"`
@@ -10,8 +13,13 @@ type Product struct {
 	Price float64 `json:"precio"`
 }
 
-var Products []Product
 var lastID int
+
+const (
+	ProductNotFound = "product %d not found"
+	FailReading     = "cant read database"
+	FailWriting     = "cant write database, error: %w"
+)
 
 type Repository interface {
 	GetAll() ([]Product, error)
@@ -22,28 +30,60 @@ type Repository interface {
 	Delete(id int) error
 }
 
-type repository struct{}
+type repository struct {
+	db store.Store
+}
 
-func NewRepository() Repository {
-	return &repository{}
+func NewRepository(db store.Store) Repository {
+	return &repository{
+		db: db,
+	}
 }
 
 func (r *repository) GetAll() ([]Product, error) {
+	var Products []Product
+	if err := r.db.Read(&Products); err != nil {
+		return nil, fmt.Errorf(FailReading)
+	}
 	return Products, nil
 }
 
 func (r *repository) LastID() (int, error) {
-	return lastID, nil
+	var Products []Product
+	if err := r.db.Read(&Products); err != nil {
+		return 0, fmt.Errorf(FailReading)
+	}
+	if len(Products) == 0 {
+		return 0, nil
+	}
+
+	return Products[len(Products)-1].ID, nil
 }
 
 func (r *repository) Store(id int, nombre, tipo string, cantidad int, precio float64) (Product, error) {
+	var Products []Product
+
+	if err := r.db.Read(&Products); err != nil {
+		return Product{}, fmt.Errorf(FailReading)
+	}
+
 	p := Product{id, nombre, tipo, cantidad, precio}
 	Products = append(Products, p)
-	lastID = p.ID
+
+	if err := r.db.Write(Products); err != nil {
+		return Product{}, fmt.Errorf(FailWriting, err)
+	}
+
 	return p, nil
 }
 
 func (r *repository) Update(id int, nombre, tipo string, cantidad int, precio float64) (Product, error) {
+	var Products []Product
+
+	if err := r.db.Read(&Products); err != nil {
+		return Product{}, fmt.Errorf(FailReading)
+	}
+
 	p := Product{Name: nombre, Type: tipo, Count: cantidad, Price: precio}
 	updated := false
 	for i := range Products {
@@ -55,13 +95,23 @@ func (r *repository) Update(id int, nombre, tipo string, cantidad int, precio fl
 	}
 
 	if !updated {
-		return Product{}, fmt.Errorf("producto %d no encontrado", id)
+		return Product{}, fmt.Errorf(ProductNotFound, id)
+	}
+
+	if err := r.db.Write(Products); err != nil {
+		return Product{}, fmt.Errorf(FailWriting, err)
 	}
 
 	return p, nil
 }
 
 func (r *repository) Patch(id int, nombre string, precio float64) (Product, error) {
+	var Products []Product
+
+	if err := r.db.Read(&Products); err != nil {
+		return Product{}, fmt.Errorf(FailReading)
+	}
+
 	updated := false
 	var p Product
 	for i := range Products {
@@ -74,13 +124,23 @@ func (r *repository) Patch(id int, nombre string, precio float64) (Product, erro
 	}
 
 	if !updated {
-		return Product{}, fmt.Errorf("producto %d no encontrado", id)
+		return Product{}, fmt.Errorf(ProductNotFound, id)
+	}
+
+	if err := r.db.Write(Products); err != nil {
+		return Product{}, fmt.Errorf(FailWriting, err)
 	}
 
 	return p, nil
 }
 
 func (r *repository) Delete(id int) error {
+	var Products []Product
+
+	if err := r.db.Read(&Products); err != nil {
+		return fmt.Errorf(FailReading)
+	}
+
 	deleted := false
 	var index int
 	for i := range Products {
@@ -91,9 +151,12 @@ func (r *repository) Delete(id int) error {
 	}
 
 	if !deleted {
-		return fmt.Errorf("producto %d no encontrado", id)
+		return fmt.Errorf(ProductNotFound, id)
 	}
 
 	Products = append(Products[:index], Products[index+1:]...)
+	if err := r.db.Write(Products); err != nil {
+		return fmt.Errorf(FailWriting, err)
+	}
 	return nil
 }
