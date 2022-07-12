a3238e4468c157275c53a89c9fd1833f1f1d144f
Luz <luz.lucumi@mercadolibre.com.co>
2022-07-12 15:04:56 -0500

    Fixing route ticket.csv

--- goweb/desafio-go-web/cmd/server/main.go
+++ goweb/desafio-go-web/cmd/server/main.go
@@ -16,7 +16,7 @@ import (
 func main() {
 
 	// Cargo csv.
-	list, err := LoadTicketsFromFile("../../tickets.csv")
+	list, err := LoadTicketsFromFile("tickets.csv")
 	if err != nil {
 		panic("Couldn't load tickets")
 	}
