13675b89a66d17bef4bae4e9f30be83933ef9eb5
crisnLopezMeli <cristian.lopez@mercadolibre.com>
2022-07-12 15:55:11 -0300

    service

--- desafio-go-web/internal/tickets/service.go
+++ desafio-go-web/internal/tickets/service.go
@@ -1,3 +1,46 @@
 package tickets
 
-type Service struct{}
+import (
+	"desafio-go-web/internal/domain"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Service interface {
+	AverageDestination(c *gin.Context, destination string) (float64, error)
+	GetTotalTickets(c *gin.Context, destination string) ([]domain.Ticket, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func (s *service) GetTotalTickets(c *gin.Context, destination string) ([]domain.Ticket, error) {
+	tickets, err := s.repository.GetTicketByDestination(c, destination)
+	if err != nil {
+		return []domain.Ticket{}, fmt.Errorf("error al obtener Tickets de %s. error: %s", destination, err)
+	}
+
+	return tickets, nil
+}
+
+func (s *service) AverageDestination(c *gin.Context, destination string) (float64, error) {
+	ticketDestination, err := s.repository.GetTicketByDestination(c, destination)
+	if err != nil {
+		return 0, fmt.Errorf("error al obtener Tickets de %s. Error: %s", destination, err)
+	}
+
+	if len(ticketDestination) == 0 {
+		return 0, fmt.Errorf("no existen tickets para %s. Imposible sacar promedio", destination)
+	}
+
+	tickets, err := s.repository.GetAll(c)
+	if err != nil {
+		return 0, fmt.Errorf("error al obtener tickets: %s", err)
+	}
+
+	avg := float64(len(ticketDestination)) / float64(len(tickets))
+
+	return avg, nil
+}
