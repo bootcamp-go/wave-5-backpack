020467a25415f3a3504ec3004bdacecbe57560b8
Daniela Bedoya Poveda <daniela.bedoya@mercadolibre.com.co>
2022-07-22 14:43:05 -0500

    Ejercicios resueltos

--- testing/C1_TT/Dividr/dividir.go
+++ testing/C1_TT/Dividr/dividir.go
@@ -2,9 +2,9 @@ package Dividr
 
 import "errors"
 
-func Dividir(num1, num2 int) (int, error) {
-	if num2 == 0 {
+func Dividir(num1, den int) (int, error) {
+	if den == 0 {
 		return 0, errors.New("el denominador no puede ser 0")
 	}
-	return num1 / num2, nil
+	return num1 / den, nil
 }
