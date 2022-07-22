0be98ce1be4f991c7788800d03d7841e42e9d848
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-22 16:45:13 -0300

    Agregando correccion a test dividir

--- testing/clase1_parte2/dividir/dividir_test.go
+++ testing/clase1_parte2/dividir/dividir_test.go
@@ -6,11 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDividir(t *testing.T) {
+func TestDividirError(t *testing.T) {
 
 	num1 := 8
 	num2 := 0
 	_, err := Dividir(num1, num2)
+	assert.EqualError(t, err, "el denominador no puede ser 0")
+}
+
+func TestDividirExito(t *testing.T) {
+
+	num1 := 8
+	num2 := 1
+	resultadoEsp := 8
+	resultado, err := Dividir(num1, num2)
 
 	assert.Nil(t, err)
+	assert.Equal(t, resultadoEsp, resultado, "deberían ser iguales")
 }
