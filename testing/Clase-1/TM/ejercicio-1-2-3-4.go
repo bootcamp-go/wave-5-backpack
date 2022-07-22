ce5578996e31afcc24fd7871532edcf3e2245301
FerRiosP <fernando.rios@mercadolibre.cl>
2022-07-22 11:00:50 -0400

    first commit testing

---
+++ testing/Clase-1/TM/ejercicio-1-2-3-4.go
@@ -0,0 +1,18 @@
+package main
+
+//Ejercicio 1
+//¿Cuáles son las diferencias entre White Box y Black Box?
+
+/*
+
+
+ */
+
+//Ejercicio 2
+//¿Qué es un test funcional?.
+
+//Ejercicio 3
+//¿Qué es un Test de Integración?
+
+//Ejercicio 4
+//Indicar las dimensiones de calidad prioritarias en MELI.
