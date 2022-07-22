c921f46d8ddbbc0d0b8fa9b7b2a7f33fe2e8043e
deroldan <denis.roldan@mercadolibre.com>
2022-07-22 12:40:40 -0300

    cuestionario testing

--- goweb/cmd/server/handler/products.go
+++ goweb/cmd/server/handler/products.go
@@ -11,7 +11,7 @@ import (
 )
 
 type request struct {
-	Nombre    string  `json:"nombre" binding:"required" `
+	Nombre    string  `json:"nombre" binding:"required"`
 	Color     string  `json:"color" binding:"required"`
 	Precio    float64 `json:"precio" binding:"required"`
 	Stock     int     `json:"stock" binding:"required"`
