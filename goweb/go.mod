5bd5ac53d8a541e7a80dbe87c1689c625835e8cd
crisnLopezMeli <cristian.lopez@mercadolibre.com>
2022-07-25 12:06:33 -0300

    test GetAll con StubRepository

--- goweb/go.mod
+++ goweb/go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/testify v1.8.0
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe
 	github.com/swaggo/gin-swagger v1.5.1
 	github.com/swaggo/swag v1.8.3
@@ -12,6 +13,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -29,6 +31,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
@@ -37,4 +40,5 @@ require (
 	golang.org/x/tools v0.1.11 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ goweb/internal/transactions/service_test.go
@@ -0,0 +1,90 @@
+package transactions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bootcamp-go/wave-5-backpack/tree/lopez_cristian/goweb/internal/models"
+)
+
+type StubRepository struct{}
+
+func (s *StubRepository) Store(monto float64, cod, moneda, emisor, receptor string) (models.Transaction, error) {
+	return models.Transaction{}, nil
+}
+
+func (s *StubRepository) Update(id int, monto float64, cod, moneda, emisor, receptor string) (models.Transaction, error) {
+	return models.Transaction{}, nil
+}
+
+func (s *StubRepository) Patch(id int, monto float64, cod, moneda, emisor, receptor string) (models.Transaction, error) {
+	return models.Transaction{}, nil
+}
+
+func (s *StubRepository) GetAll() ([]models.Transaction, error) {
+	return []models.Transaction{
+		{
+			ID:       1,
+			Monto:    500.5,
+			Cod:      "aaa111",
+			Moneda:   "ARS",
+			Emisor:   "Mercado Pago",
+			Receptor: "BBVA",
+			Fecha:    "2022-07-25 12:00",
+		},
+		{
+			ID:       2,
+			Monto:    1000,
+			Cod:      "aaa112",
+			Moneda:   "ARS",
+			Emisor:   "BBVA",
+			Receptor: "Mercado Pago",
+			Fecha:    "2022-07-25 12:00",
+		},
+	}, nil
+}
+
+func (s *StubRepository) GetByID(id int) (models.Transaction, error) {
+	return models.Transaction{}, nil
+}
+
+func (s *StubRepository) GetLastID() (int, error) {
+	return 0, nil
+}
+
+func (s *StubRepository) Delete(id int) (int, error) {
+	return 0, nil
+}
+
+func TestGetAll(t *testing.T) {
+	// Arrange
+	service := NewService(&StubRepository{})
+	transactionsExpected := []models.Transaction{
+		{
+			ID:       1,
+			Monto:    500.5,
+			Cod:      "aaa111",
+			Moneda:   "ARS",
+			Emisor:   "Mercado Pago",
+			Receptor: "BBVA",
+			Fecha:    "2022-07-25 12:00",
+		},
+		{
+			ID:       2,
+			Monto:    1000,
+			Cod:      "aaa112",
+			Moneda:   "ARS",
+			Emisor:   "BBVA",
+			Receptor: "Mercado Pago",
+			Fecha:    "2022-07-25 12:00",
+		},
+	}
+
+	// Act
+	transactions, err := service.GetAll()
+
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, transactionsExpected, transactions)
+}
