2cbd1854fbfaef3b37a1dc048991c6cfabcc62a5
Juan Pablo Ortiz Rubio <juanpablo.ortiz@mercadolibre.com.co>
2022-07-25 14:12:35 -0500

    Go Testing Clase 2 Parte 1 - Ejercicios

---
+++ goweb/internal/products/repository_mock_test.go
@@ -0,0 +1,56 @@
+package products
+
+import (
+	"goweb/internal/domain"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Stub Struct
+
+type MockStore struct {
+	ReadWasCalled bool
+}
+
+func (m *MockStore) Read(data interface{}) error {
+	m.ReadWasCalled = true
+
+	products := data.(*[]domain.Product)
+	*products = append(*products, domain.Product{Id: 1, Nombre: "Before Update", Color: "rojo", Precio: 10, Stock: 10, Codigo: "123", Publicado: true, FechaCreacion: "2020-01-01"})
+	return nil
+}
+
+func (s *MockStore) Write(data interface{}) error {
+	return nil
+}
+
+func (s *MockStore) Ping() error {
+	return nil
+}
+
+// Test
+
+func TestUpdateName(t *testing.T) {
+	// Se inicializan los datos a usar en el test (input y output)
+	myMockStore := MockStore{}
+	myRepository := NewRepositoryJsonCorrDB(&myMockStore)
+	resultadoEsperado := domain.Product{
+		Id:            1,
+		Nombre:        "After Update",
+		Color:         "rojo",
+		Precio:        10,
+		Stock:         10,
+		Codigo:        "123",
+		Publicado:     true,
+		FechaCreacion: "2020-01-01",
+	}
+
+	// Se ejecuta el test
+	resultadoObtenido, err := myRepository.UpdateNombre(1, "After Update")
+
+	// Se compara el resultado obtenido con el resultado esperado
+	assert.Nil(t, err)
+	assert.Equal(t, resultadoEsperado, resultadoObtenido)
+	assert.True(t, myMockStore.ReadWasCalled)
+}
---
+++ goweb/internal/products/repository_stub_test.go
@@ -0,0 +1,46 @@
+package products
+
+import (
+	"goweb/internal/domain"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Stub Struct
+
+type StubStore struct{}
+
+func (s *StubStore) Read(data interface{}) error {
+	products := data.(*[]domain.Product)
+	*products = append(*products, domain.Product{Id: 1, Nombre: "producto 1", Color: "rojo", Precio: 10, Stock: 10, Codigo: "123", Publicado: true, FechaCreacion: "2020-01-01"})
+	*products = append(*products, domain.Product{Id: 2, Nombre: "producto 2", Color: "rojo", Precio: 10, Stock: 10, Codigo: "123", Publicado: true, FechaCreacion: "2020-01-01"})
+	return nil
+}
+
+func (s *StubStore) Write(data interface{}) error {
+	return nil
+}
+
+func (s *StubStore) Ping() error {
+	return nil
+}
+
+// Test
+
+func TestGetAll(t *testing.T) {
+	// Se inicializan los datos a usar en el test (input y output)
+	myStubStore := StubStore{}
+	myRepository := NewRepositoryJsonCorrDB(&myStubStore)
+	resultadoEsperado := []domain.Product{
+		{Id: 1, Nombre: "producto 1", Color: "rojo", Precio: 10, Stock: 10, Codigo: "123", Publicado: true, FechaCreacion: "2020-01-01"},
+		{Id: 2, Nombre: "producto 2", Color: "rojo", Precio: 10, Stock: 10, Codigo: "123", Publicado: true, FechaCreacion: "2020-01-01"},
+	}
+
+	// Se ejecuta el test
+	resultadoObtenido, err := myRepository.GetAll()
+
+	// Se compara el resultado obtenido con el resultado esperado
+	assert.Nil(t, err)
+	assert.Equal(t, resultadoEsperado, resultadoObtenido)
+}
