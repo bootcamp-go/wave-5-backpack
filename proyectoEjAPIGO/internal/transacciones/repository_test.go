90ffb20d4941c5399dc307185e22b83f01d19d73
joakinalvarz <joaquin.alvarez@mercadolibre.com>
2022-07-25 13:31:50 -0300

    push stub test

---
+++ proyectoEjAPIGO/internal/transacciones/repository_test.go
@@ -0,0 +1,90 @@
+package transacciones
+
+import (
+	"goweb/internal/domain"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Stub Test
+type StubRepository struct{}
+
+func (s *StubRepository) Store(id int, codigo_transaccion, moneda, emisor, receptor, fecha_transaccion string, monto float64) (domain.Transaccion, error) {
+	return domain.Transaccion{}, nil
+}
+
+func (s *StubRepository) Update(id int, codigo_transaccion, moneda, emisor, receptor, fecha_transaccion string, monto float64) (domain.Transaccion, error) {
+	return domain.Transaccion{}, nil
+}
+
+func (s *StubRepository) UpdateCTandMonto(id int, codigo_transaccion string, monto float64) (domain.Transaccion, error) {
+	return domain.Transaccion{}, nil
+}
+
+func (s *StubRepository) GetAll() ([]domain.Transaccion, error) {
+	return []domain.Transaccion{
+		{
+			ID:                 1,
+			Monto:              500.5,
+			Codigo_transaccion: "aaa111",
+			Moneda:             "ARS",
+			Emisor:             "Mercado Pago",
+			Receptor:           "BBVA",
+			Fecha_transaccion:  "2022-07-25 12:00",
+		},
+		{
+			ID:                 2,
+			Monto:              1000,
+			Codigo_transaccion: "aaa112",
+			Moneda:             "ARS",
+			Emisor:             "BBVA",
+			Receptor:           "Mercado Pago",
+			Fecha_transaccion:  "2022-07-25 12:00",
+		},
+	}, nil
+}
+
+func (s *StubRepository) GetByID(id int) (domain.Transaccion, error) {
+	return domain.Transaccion{}, nil
+}
+
+func (s *StubRepository) LastID() (int, error) {
+	return 0, nil
+}
+
+func (s *StubRepository) Delete(id int) error {
+	return nil
+}
+
+func TestGetAll(t *testing.T) {
+	// Arrange
+	service := NewService(&StubRepository{})
+	transactionsExpected := []domain.Transaccion{
+		{
+			ID:                 1,
+			Monto:              500.5,
+			Codigo_transaccion: "aaa111",
+			Moneda:             "ARS",
+			Emisor:             "Mercado Pago",
+			Receptor:           "BBVA",
+			Fecha_transaccion:  "2022-07-25 12:00",
+		},
+		{
+			ID:                 2,
+			Monto:              1000,
+			Codigo_transaccion: "aaa112",
+			Moneda:             "ARS",
+			Emisor:             "BBVA",
+			Receptor:           "Mercado Pago",
+			Fecha_transaccion:  "2022-07-25 12:00",
+		},
+	}
+
+	// Act
+	transactions, err := service.GetAll()
+
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, transactionsExpected, transactions)
+}
