68eecd0820d37c093d4b2e6d544879e05021cbf8
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-25 12:57:55 -0300

    Mock and Struct Test

---
+++ goweb/internal/usuarios/service_mock_test.go
@@ -0,0 +1,63 @@
+package usuarios
+
+import (
+	"testing"
+
+	"github.com/bootcamp-go/wave-5-backpack/goweb/internal/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockDB struct {
+	GetWasCalled bool
+}
+
+func (mdb *MockDB) GetAll() ([]domain.Usuarios, error) {
+	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
+	return sliceUsers, nil
+}
+
+func (mdb *MockDB) GetById(id int) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+
+func (mdb *MockDB) UpdateNameAndLastName(id int, name string, last string) (domain.Usuarios, error) {
+	BeforeUpdate, _ := mdb.GetAll()
+	mdb.GetWasCalled = true
+	var user domain.Usuarios
+	for i := 0; i < len(BeforeUpdate)-1; i++ {
+		if BeforeUpdate[i].Id == id {
+			user = BeforeUpdate[i]
+			user.Nombre = name
+			user.Apellido = last
+		}
+	}
+
+	return user, nil
+}
+
+func (mdb *MockDB) Update(id int, nombre, apellido, email string, edad int, altura float64, activo bool, fecha string) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+
+func (mdb *MockDB) Delete(id int) error {
+	return nil
+}
+
+func (mdb *MockDB) Guardar(id int, nombre string, apellido string, email string, edad int, altura float64, actico bool, fecha string) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+func (mdb *MockDB) LastId() (int, error) {
+	return 0, nil
+}
+
+func TestUpdate(t *testing.T) {
+	myMock := MockDB{}
+	servi := NewService(&myMock)
+
+	user1 := domain.Usuarios{Id: 1, Nombre: "Perla", Apellido: "Oceano", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}
+
+	resultado, _ := servi.UpdateNameAndLastName(1, "Perla", "Oceano")
+
+	assert.Equal(t, user1, resultado)
+	assert.True(t, myMock.GetWasCalled)
+}
---
+++ goweb/internal/usuarios/service_test.go
@@ -0,0 +1,50 @@
+package usuarios
+
+import (
+	"testing"
+
+	"github.com/bootcamp-go/wave-5-backpack/goweb/internal/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+type StubDB struct{}
+
+func (sdb *StubDB) GetAll() ([]domain.Usuarios, error) {
+	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
+	return sliceUsers, nil
+}
+
+func (sdb *StubDB) GetById(id int) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+
+func (sdb *StubDB) UpdateNameAndLastName(id int, name string, last string) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+
+func (sdb *StubDB) Update(id int, nombre, apellido, email string, edad int, altura float64, activo bool, fecha string) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+
+func (sdb *StubDB) Delete(id int) error {
+	return nil
+}
+
+func (sdb *StubDB) Guardar(id int, nombre string, apellido string, email string, edad int, altura float64, actico bool, fecha string) (domain.Usuarios, error) {
+	return domain.Usuarios{}, nil
+}
+func (sdb *StubDB) LastId() (int, error) {
+	return 0, nil
+}
+
+func TestGetAll(t *testing.T) {
+	myStubDB := StubDB{}
+	servi := NewService(&myStubDB)
+
+	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
+
+	resultado, err := servi.GetAll()
+
+	assert.Equal(t, sliceUsers, resultado)
+	assert.Nil(t, err)
+}
