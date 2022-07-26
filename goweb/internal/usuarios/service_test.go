e3fbc283f2a210ff1784b22b64ae13b58c9bd63f
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-26 19:07:02 -0300

    Coverage 100 in service

--- goweb/internal/usuarios/service_test.go
+++ goweb/internal/usuarios/service_test.go
@@ -2,6 +2,7 @@ package usuarios
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/bootcamp-go/wave-5-backpack/goweb/internal/domain"
@@ -11,6 +12,7 @@ import (
 type StubDB struct {
 	errRead string
 	errLast string
+	errSave string
 }
 
 func (sdb *StubDB) GetAll() ([]domain.Usuarios, error) {
@@ -22,7 +24,13 @@ func (sdb *StubDB) GetAll() ([]domain.Usuarios, error) {
 }
 
 func (sdb *StubDB) GetById(id int) (domain.Usuarios, error) {
-	return domain.Usuarios{}, nil
+	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
+	for i := 0; i < len(sliceUsers); i++ {
+		if sliceUsers[i].Id == id {
+			return sliceUsers[i], nil
+		}
+	}
+	return domain.Usuarios{}, errors.New("error al encontrar el user")
 }
 
 func (sdb *StubDB) UpdateNameAndLastName(id int, name string, last string) (domain.Usuarios, error) {
@@ -41,6 +49,9 @@ func (sdb *StubDB) Guardar(id int, nombre string, apellido string, email string,
 	if sdb.errLast != "" {
 		return domain.Usuarios{}, errors.New("error in last")
 	}
+	if sdb.errSave != "" {
+		return domain.Usuarios{}, errors.New("error saving")
+	}
 	user := domain.Usuarios{
 		Id:            id,
 		Nombre:        nombre,
@@ -54,6 +65,9 @@ func (sdb *StubDB) Guardar(id int, nombre string, apellido string, email string,
 	return user, nil
 }
 func (sdb *StubDB) LastId() (int, error) {
+	if sdb.errLast != "" {
+		return 0, errors.New("error in last id")
+	}
 	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
 	lastId := len(sliceUsers)
 	return lastId, nil
@@ -91,13 +105,46 @@ func TestGuardar(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-func TestGuardarError(t *testing.T) {
+func TestGuardarErrorLastID(t *testing.T) {
+	writeLast := fmt.Errorf("error in last id")
+	expectedError := fmt.Errorf("error al obtener usuario con id: %w", writeLast)
 	myStubDB := StubDB{
-		errLast: "error in last",
+		errLast: "error in last id",
 	}
 	servi := NewService(&myStubDB)
 	result, err := servi.Guardar("new", "new", "new", 20, 20, true, "2020")
 
-	assert.EqualError(t, err, myStubDB.errLast)
+	assert.Equal(t, err, expectedError)
+	assert.Equal(t, domain.Usuarios{}, result)
+}
+func TestGuardarError(t *testing.T) {
+	expectedError := fmt.Errorf("error saving")
+	myStubDB := StubDB{
+		errSave: "error saving",
+	}
+	servi := NewService(&myStubDB)
+	result, err := servi.Guardar("new", "new", "new", 20, 20, true, "2020")
+
+	assert.Equal(t, err, expectedError)
+	assert.Equal(t, domain.Usuarios{}, result)
+}
+
+func TestGetById(t *testing.T) {
+	myStubDB := StubDB{}
+
+	user := domain.Usuarios{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}
+	servi := NewService(&myStubDB)
+	result, err := servi.GetById(1)
+
+	assert.Equal(t, result, user)
+	assert.Nil(t, err)
+}
+func TestGetByIdNotFound(t *testing.T) {
+	expectedError := "error al encontrar el user"
+	myStubDB := StubDB{}
+	servi := NewService(&myStubDB)
+	result, err := servi.GetById(12)
+
 	assert.Equal(t, result, domain.Usuarios{})
+	assert.EqualError(t, err, expectedError)
 }
