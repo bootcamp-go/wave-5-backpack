00cf672145d76fed660089158646c4f7d944e688
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-26 17:43:34 -0300

    C3 TT EJ3

--- goweb/internal/usuarios/service_test.go
+++ goweb/internal/usuarios/service_test.go
@@ -1,15 +1,22 @@
 package usuarios
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/bootcamp-go/wave-5-backpack/goweb/internal/domain"
 	"github.com/stretchr/testify/assert"
 )
 
-type StubDB struct{}
+type StubDB struct {
+	errRead string
+	errLast string
+}
 
 func (sdb *StubDB) GetAll() ([]domain.Usuarios, error) {
+	if sdb.errRead != "" {
+		return nil, errors.New("fail to getAll")
+	}
 	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
 	return sliceUsers, nil
 }
@@ -31,15 +38,30 @@ func (sdb *StubDB) Delete(id int) error {
 }
 
 func (sdb *StubDB) Guardar(id int, nombre string, apellido string, email string, edad int, altura float64, actico bool, fecha string) (domain.Usuarios, error) {
-	return domain.Usuarios{}, nil
+	if sdb.errLast != "" {
+		return domain.Usuarios{}, errors.New("error in last")
+	}
+	user := domain.Usuarios{
+		Id:            id,
+		Nombre:        nombre,
+		Apellido:      apellido,
+		Email:         email,
+		Edad:          edad,
+		Altura:        altura,
+		Activo:        actico,
+		FechaCreacion: fecha,
+	}
+	return user, nil
 }
 func (sdb *StubDB) LastId() (int, error) {
-	return 0, nil
+	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
+	lastId := len(sliceUsers)
+	return lastId, nil
 }
 
 func TestGetAll(t *testing.T) {
 	myStubDB := StubDB{}
-	servi := NewService(&myStubDB) //Probando el service, yo le paso datos truchos a lo q quiero probar
+	servi := NewService(&myStubDB) //Probando el service, yo le paso un mock de repo a lo q quiero probar
 
 	sliceUsers := []domain.Usuarios{{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Email: "yvo", Edad: 30, Altura: 3, Activo: true, FechaCreacion: "1992"}, {Id: 2, Nombre: "Mat", Apellido: "Fant", Email: "mat", Edad: 33, Altura: 3, Activo: true, FechaCreacion: "1990"}}
 
@@ -47,4 +69,35 @@ func TestGetAll(t *testing.T) {
 
 	assert.Equal(t, sliceUsers, resultado)
 	assert.Nil(t, err)
+}
+
+func TestGetAllErrRead(t *testing.T) {
+	myStubDB := StubDB{
+		errRead: "fail to getAll",
+	}
+	servi := NewService(&myStubDB)
+	_, err := servi.GetAll()
+
+	assert.EqualError(t, err, myStubDB.errRead)
+}
+
+func TestGuardar(t *testing.T) {
+	myStubDB := StubDB{}
+	servi := NewService(&myStubDB)
+	result, err := servi.Guardar("new", "new", "new", 20, 20, true, "2020")
+
+	expected := domain.Usuarios{Id: 3, Nombre: "new", Apellido: "new", Email: "new", Edad: 20, Altura: 20, Activo: true, FechaCreacion: "2020"}
+	assert.Equal(t, result, expected)
+	assert.Nil(t, err)
+}
+
+func TestGuardarError(t *testing.T) {
+	myStubDB := StubDB{
+		errLast: "error in last",
+	}
+	servi := NewService(&myStubDB)
+	result, err := servi.Guardar("new", "new", "new", 20, 20, true, "2020")
+
+	assert.EqualError(t, err, myStubDB.errLast)
+	assert.Equal(t, result, domain.Usuarios{})
 }
