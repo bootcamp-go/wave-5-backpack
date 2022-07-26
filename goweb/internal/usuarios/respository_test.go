c942ec4bdd49781f3d7f0e512ae74c9d411c50cd
ympintos <yvonne.pintos@mercadolibre.com>
2022-07-26 19:15:28 -0300

    Coveraged updated in repository

--- goweb/internal/usuarios/respository_test.go
+++ goweb/internal/usuarios/respository_test.go
@@ -1,6 +1,7 @@
 package usuarios
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/bootcamp-go/wave-5-backpack/goweb/internal/domain"
@@ -8,6 +9,7 @@ import (
 )
 
 type StubStore struct {
+	errRead string
 }
 
 func (ss *StubStore) Validate() error {
@@ -19,6 +21,9 @@ func (ss *StubStore) Write(data interface{}) error {
 }
 
 func (ss *StubStore) Read(data interface{}) error {
+	if ss.errRead != "" {
+		return errors.New("error al leer la bd")
+	}
 	a := data.(*[]domain.Usuarios) //ACA ESTOY RECIBIENDO DESDE REPOSITORY UN PUNTERO DE LISTA DE USUARIOS
 	*a = []domain.Usuarios{        //ACA LLENO ESOS VALORES DEL PUNTERO, por eso lo desreferencio
 		{Id: 1, Nombre: "Yvo", Apellido: "Pintos", Altura: 3, FechaCreacion: "1992"},
@@ -40,4 +45,17 @@ func TestGetAllRepo(t *testing.T) {
 
 	assert.Equal(t, user, expected)
 	assert.Nil(t, err)
+}
+
+func TestGetAllRepoErrRead(t *testing.T) {
+	myStubStore := StubStore{
+		errRead: "error",
+	}
+	repo := NewRepository(&myStubStore) //Probando el repository, yo le paso datos dummy a lo que quiero probar
+	expected := "error al leer la bd"
+
+	user, err := repo.GetAll()
+
+	assert.EqualError(t, err, expected)
+	assert.Nil(t, user)
 }
