f4c05bf3f393871dc0e2b5565984b6580dbb7706
anigrelliml <agustin.nigrelli@mercadolibre.com>
2022-07-26 10:24:37 -0300

    minor refactor in store_mock.go

--- goweb/4/tt/internal/repository/store_mock.go
+++ goweb/4/tt/internal/repository/store_mock.go
@@ -8,18 +8,18 @@ import (
 type MockStore struct {
 	ReadWasCalled  bool
 	WriteWasCalled bool
+	mockProducts   []domain.Product
 }
-
-var mockProducts []domain.Product = append(products, domain.NewProduct(1, "Before Update", 1.5, 2))
 
 func (m *MockStore) Read(data interface{}) error {
 	m.ReadWasCalled = true
-	jsonProducts, _ := json.Marshal(mockProducts)
+	m.mockProducts = append(m.mockProducts, domain.NewProduct(1, "Before Update", 1.5, 2))
+	jsonProducts, _ := json.Marshal(m.mockProducts)
 	return json.Unmarshal(jsonProducts, &data)
 }
 
 func (m *MockStore) Write(data interface{}) error {
 	m.WriteWasCalled = true
 	jsonData, _ := json.Marshal(data)
-	return json.Unmarshal(jsonData, &mockProducts)
+	return json.Unmarshal(jsonData, &m.mockProducts)
 }
