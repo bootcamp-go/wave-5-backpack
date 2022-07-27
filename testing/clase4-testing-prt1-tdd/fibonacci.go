0aa743a5a88770acf6427659be45299f32e4254c
Israel Fabela <israel.fabela@mercadolibre.com.mx>
2022-07-27 11:49:47 -0500

    Update Testing | Clase4 - Practica 1 - Ejercicio 3

---
+++ testing/clase4-testing-prt1-tdd/fibonacci.go
@@ -0,0 +1,8 @@
+package clase4testingprt1tdd
+
+func FibonacciRecursion(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
+}
---
+++ testing/clase4-testing-prt1-tdd/fibonacci_test.go
@@ -0,0 +1,17 @@
+package clase4testingprt1tdd
+
+import "testing"
+
+func TestFibonacci(t *testing.T) {
+	tests := []struct {
+		arg  int
+		want int
+	}{{1, 1}, {2, 1}, {3, 2}, {5, 5}, {8, 21}, {13, 233}}
+	for i, d := range tests {
+		got := FibonacciRecursion(d.arg)
+		if got != d.want {
+			t.Errorf("Test[%d]: fibonacci(%d) returned %d, want %d",
+				i, d.arg, got, d.want)
+		}
+	}
+}
