b9a51dfc6dd5fa0128074d95f726037bd9338d02
andreramos2 <andres.ramos@mercadolibre.com.co>
2022-07-27 11:08:56 -0500

    Improvements in code

--- testing/Clase 4/Parte 1/ej 3/fibo.go
+++ testing/Clase 4/Parte 1/ej 3/fibo.go
@@ -4,11 +4,10 @@ func Fibonacci(n int) int {
 	if n == 0 {
 		return 0
 	}
-	a := 0
-	b := 1
+	a, b := 0, 1
 
-	for i := n; i > 0; i-- {
+	for i := n - 1; i > 0; i-- {
 		a, b = b, b+a
 	}
-	return a
+	return b
 }
