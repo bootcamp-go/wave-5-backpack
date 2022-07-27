310d6f469d74542584bad2df377a8eadf1c40a02
andreramos2 <andres.ramos@mercadolibre.com.co>
2022-07-27 10:58:17 -0500

    First test case

---
+++ testing/Clase 4/Parte 1/ej 3/fibo.go
@@ -0,0 +1,8 @@
+package fibo
+
+func Fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return 0
+}
---
+++ testing/Clase 4/Parte 1/ej 3/fibo_test.go
@@ -0,0 +1,20 @@
+package fibo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFibo(t *testing.T) {
+	tests := []struct {
+		given    int
+		expected int
+	}{
+		{0, 0},
+	}
+	for _, test := range tests {
+		res := Fibonacci(test.given)
+		assert.Equal(t, test.expected, res)
+	}
+}
