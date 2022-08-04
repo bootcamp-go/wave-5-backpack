0c1cd37586a4de565a5a842f9a14015c492cd26b
andreramos2 <andres.ramos@mercadolibre.com.co>
2022-08-04 11:42:40 -0500

    Ejercicio Clase 3 Pt 1

---
+++ sql/Clase 3/Parte 1/ej.sql
@@ -0,0 +1,33 @@
+-- 2.
+INSERT INTO movies 
+(id, title, rating, awards, release_date, length, genre_id) 
+VALUES (22, 'Deadpool', 9.2, 3, STR_TO_DATE('2018-04-12', '%Y-%m-%d'), 100, 4);
+-- 3 
+INSERT INTO genres
+(id, name, ranking)
+VALUES (13, 'Violencia', 13);
+-- 4
+UPDATE movies m
+SET m.genre_id = 13
+WHERE m.id = 22;
+-- 5
+UPDATE actors a
+SET a.favorite_movie_id = 22
+WHERE a.id = 3;
+-- 6 
+CREATE TEMPORARY TABLE temp_movies (SELECT * FROM movies);
+-- 7
+DELETE FROM temp_movies tmpm WHERE tmpm.awards < 5;
+-- 8 
+SELECT g.id, g.name, COUNT(m.id) conteo
+FROM genres g
+JOIN movies m
+WHERE g.id = m.genre_id
+GROUP BY m.genre_id
+HAVING conteo >= 1;
+-- 9
+SELECT a.first_name, a.last_name, m.awards
+FROM actors a
+JOIN movies m
+WHERE a.favorite_movie_id = m.id
+AND m.awards > 3;
---
+++ sql/Clase 3/Parte 1/preguntas.md
@@ -0,0 +1,6 @@
+# Preguntas
+## 1. Explicar el concepto de normalización y para que se utiliza.
+Consiste en diseñar una base de datos con el objetivo de minimizar los datos repetidos
+## 11. ¿Qué son los índices? ¿Para qué sirven?
+- Los indices son mecanismos para optimizar ciertos tipos de consultas en SQL
+- Los indices sirven para que consultas relacionadas con la busqueda de datos funcionen mas rapido 
