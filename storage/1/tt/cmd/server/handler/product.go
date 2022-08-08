8d78e3fde2da71536634cd9a7d3dc3e77e5e2e09
anigrelliml <agustin.nigrelli@mercadolibre.com>
2022-08-08 18:31:16 -0300

    added Storage I tt

---
+++ storage/1/tt/cmd/server/handler/product.go
@@ -0,0 +1,169 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"storage/1/tt/internal/product"
+	"storage/1/tt/pkg/web"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type Product struct {
+	s product.Service
+}
+
+type request struct {
+	Name  string  `json:"name" binding:"required"`
+	Type  string  `json:"type" binding:"required"`
+	Price float64 `json:"price" binding:"required"`
+	Count int     `json:"count" binding:"required"`
+}
+
+func NewProduct(s product.Service) *Product {
+	return &Product{s: s}
+}
+
+func (h *Product) Read() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			code := http.StatusBadRequest
+			ctx.JSON(code, web.NewResponse(code, nil, "id invalido"))
+			return
+		}
+
+		product, err := h.s.Read(id)
+		if err != nil {
+			code := http.StatusInternalServerError
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusOK
+		ctx.JSON(code, web.NewResponse(code, product, ""))
+	}
+}
+
+func (h *Product) ReadByName() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		name := ctx.Param("name")
+
+		products, err := h.s.ReadByName(name)
+		if err != nil {
+			code := http.StatusNotFound
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusOK
+		ctx.JSON(code, web.NewResponse(code, products, ""))
+	}
+}
+
+func (h *Product) ReadAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		products, err := h.s.ReadAll()
+		if err != nil {
+			code := http.StatusInternalServerError
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusOK
+		ctx.JSON(code, web.NewResponse(code, products, ""))
+	}
+}
+
+func (h *Product) Create() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req request
+
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var ve validator.ValidationErrors
+			var notBinded string
+			if errors.As(err, &ve) {
+				notBinded += "Los siguientes campos son requeridos: "
+				for _, fe := range ve {
+					notBinded += fmt.Sprintf("%s ", fe.Field())
+				}
+			}
+
+			code := http.StatusBadRequest
+			ctx.JSON(code, web.NewResponse(code, nil, notBinded))
+			return
+		}
+
+		product, err := h.s.Create(req.Name, req.Type, req.Price, req.Count)
+		if err != nil {
+			code := http.StatusInternalServerError
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusCreated
+		ctx.JSON(code, web.NewResponse(code, product, ""))
+	}
+}
+
+func (h *Product) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			code := http.StatusBadRequest
+			ctx.JSON(code, web.NewResponse(code, nil, "id invalido"))
+			return
+		}
+
+		var req request
+
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var ve validator.ValidationErrors
+			var notBinded string
+			if errors.As(err, &ve) {
+				notBinded += "Los siguientes campos son requeridos: "
+				for _, fe := range ve {
+					notBinded += fmt.Sprintf("%s ", fe.Field())
+				}
+			}
+
+			code := http.StatusBadRequest
+			ctx.JSON(code, web.NewResponse(code, nil, notBinded))
+			return
+		}
+
+		product, err := h.s.Update(id, req.Name, req.Type, req.Price, req.Count)
+		if err != nil {
+			code := http.StatusInternalServerError
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusOK
+		ctx.JSON(code, web.NewResponse(code, product, ""))
+	}
+}
+
+func (h *Product) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			code := http.StatusBadRequest
+			ctx.JSON(code, web.NewResponse(code, nil, "id invalido"))
+			return
+		}
+
+		err = h.s.Delete(id)
+		if err != nil {
+			code := http.StatusInternalServerError
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		code := http.StatusOK
+		ctx.JSON(code, web.NewResponse(code, fmt.Sprintf("producto de id %d borrado", id), ""))
+	}
+}
---
+++ storage/1/tt/internal/product/repository.go
@@ -0,0 +1,139 @@
+package product
+
+import (
+	"database/sql"
+	"log"
+	"storage/1/tt/internal/domain"
+)
+
+type Repository interface {
+	Store(product domain.Product) (domain.Product, error)
+	GetAll() ([]domain.Product, error)
+	GetOne(id int) (domain.Product, error)
+	GetByName(name string) ([]domain.Product, error)
+	Update(product domain.Product) (domain.Product, error)
+	Delete(id int) error
+}
+
+type repository struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) Repository {
+	return &repository{db: db}
+}
+
+const (
+	StoreProduct   = "INSERT INTO products(name, type, count, price) VALUES (?, ?, ?, ?)"
+	GetAllProducts = "SELECT * FROM products"
+	GetProduct     = "SELECT * FROM products WHERE id = ?"
+	GetByName      = "SELECT * FROM products WHERE name = ?"
+	UpdateProduct  = "UPDATE produtcs SET name = ?, type = ?, count = ?, price = ?, WHERE id = ?"
+	DeleteProduct  = "DELETE FROM products WHERE id = ?"
+)
+
+func (r *repository) Store(product domain.Product) (domain.Product, error) {
+	stmt, err := r.db.Prepare(StoreProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	var result sql.Result
+	result, err = stmt.Exec(product.Name, product.Type, product.Count, product.Price)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	insertedID, _ := result.LastInsertId()
+	product.ID = int(insertedID)
+
+	return product, nil
+}
+
+func (r *repository) GetOne(id int) (domain.Product, error) {
+	var product domain.Product
+	rows, err := r.db.Query(GetProduct, id)
+	if err != nil {
+		log.Println(err)
+		return domain.Product{}, err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+			log.Println(err)
+			return domain.Product{}, err
+		}
+	}
+
+	return product, nil
+}
+
+func (r *repository) GetByName(name string) ([]domain.Product, error) {
+	var product domain.Product
+	var products []domain.Product
+	rows, err := r.db.Query(GetByName, name)
+	if err != nil {
+		log.Println(err)
+		return []domain.Product{}, err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+			log.Println(err)
+			return []domain.Product{}, err
+		}
+		products = append(products, product)
+	}
+
+	return products, nil
+}
+
+func (r *repository) GetAll() ([]domain.Product, error) {
+	var product domain.Product
+	var products []domain.Product
+	rows, err := r.db.Query(GetAllProducts)
+	if err != nil {
+		log.Println(err)
+		return []domain.Product{}, err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+			log.Println(err)
+			return []domain.Product{}, err
+		}
+		products = append(products, product)
+	}
+
+	return products, nil
+}
+
+func (r *repository) Update(product domain.Product) (domain.Product, error) {
+	stmt, err := r.db.Prepare(UpdateProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Type, product.Type, product.Count, product.Price, product.ID)
+	if err != nil {
+		return domain.Product{}, err
+	}
+
+	return product, nil
+}
+
+func (r *repository) Delete(id int) error {
+	stmt, err := r.db.Prepare(DeleteProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
