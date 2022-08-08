f985e49f238b696ae8f6c5d5c30e5fd37553113e
andreramos2 <andres.ramos@mercadolibre.com.co>
2022-08-08 16:27:46 -0500

    Nuevo Repository.Store usando MySQL

--- storage/Clase 1/Parte 2/internal/products/repository.go
+++ storage/Clase 1/Parte 2/internal/products/repository.go
@@ -25,8 +25,27 @@ func NewRepository(db *sql.DB) Repository {
 	}
 }
 
-func (r *repository) Store(domain.Product) (domain.Product, error) {
-	return domain.Product{}, nil
+func (r *repository) Store(product domain.Product) (domain.Product, error) {
+	db := r.db
+	stmt, err := db.Prepare(
+		"INSERT INTO PRODUCTS " +
+			"(Name, Color, Price, Stock, Code, Published, Created_at)" +
+			" VALUES (?, ?, ?, ?, ?, ?, CURDATE())",
+	)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	defer stmt.Close()
+	sqlRes, err := stmt.Exec(product.Name, product.Color, product.Price, product.Stock, product.Code, product.Published)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	insertedId, err := sqlRes.LastInsertId()
+	if err != nil {
+		return domain.Product{}, err
+	}
+	product.Id = uint64(insertedId)
+	return product, nil
 }
 
 func (r *repository) GetAll() ([]domain.Product, error) {
