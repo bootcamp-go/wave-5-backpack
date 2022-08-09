41266bb6a8056b7b6e1a0fdcf45a962f66d3aa53
Jessicaescobarmeli <jessica.escobar@mercadolibre.com.co>
2022-08-09 17:41:09 -0500

    Practica 2 | Clase 2 | Implementacion de Storage

--- storage/products_project/go.mod
+++ storage/products_project/go.mod
@@ -3,7 +3,10 @@ module products_project
 go 1.18
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.8.0
 	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a
@@ -12,12 +15,9 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-txdb v0.1.5 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -26,7 +26,6 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -37,12 +36,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	github.com/urfave/cli/v2 v2.11.1 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
 	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
--- storage/products_project/internal/products/repository_test.go
+++ storage/products_project/internal/products/repository_test.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"database/sql"
 	"products_project/internal/domain"
+	"regexp"
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/DATA-DOG/go-txdb"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,4 +34,283 @@ func TestGetAll(t *testing.T) {
 	}
 	assert.Nil(t, err)
 	assert.Equal(t, expectedProducts, products)
+}
+
+func TestRepositoryStore(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mock.ExpectPrepare("INSERT INTO products")
+	mock.ExpectExec("INSERT INTO products").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	productId := 1
+
+	repo := NewRepository(db)
+	product := domain.Product{
+		Id:        productId,
+		Nombre:    "Avion",
+		Color:     "Blanco",
+		Precio:    788,
+		Stock:     8,
+		Codigo:    "AF456",
+		Publicado: true,
+		Fecha:     "02-09-22",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Store(ctx, product.Nombre, product.Color, product.Precio, product.Stock, product.Codigo, product.Publicado, product.Fecha)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, product, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestRepositoryStoreTXDB(t *testing.T) {
+	dataSource := "root@tcp(localhost:3306)/storage"
+	txdb.Register("txdb_store", "mysql", dataSource)
+	db, err := sql.Open("txdb_store", uuid.New().String())
+	assert.NoError(t, err)
+
+	repo := NewRepository(db)
+	product := domain.Product{
+		Nombre:    "Avion",
+		Color:     "Blanco",
+		Precio:    788,
+		Stock:     8,
+		Codigo:    "AF456",
+		Publicado: true,
+		Fecha:     "02-09-22",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Store(ctx, product.Nombre, product.Color, product.Precio, product.Stock, product.Codigo, product.Publicado, product.Fecha)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+}
+
+func TestRepositoryGetById(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	productId := 6
+	columns := []string{"id", "nombre", "color", "precio", "stock", "codigo", "publicado", "fecha"}
+	rows := sqlmock.NewRows(columns)
+	rows.AddRow(productId, "Carro", "Azul", 300, 6, "C3456", true, "23-04-12")
+	mock.ExpectQuery(regexp.QuoteMeta(GetById)).WillReturnRows(rows)
+	repo := NewRepository(db)
+	expectedProduct := domain.Product{
+		Id:        productId,
+		Nombre:    "Carro",
+		Color:     "Azul",
+		Precio:    300,
+		Stock:     6,
+		Codigo:    "C3456",
+		Publicado: true,
+		Fecha:     "23-04-12",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.GetById(ctx, productId)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, expectedProduct, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestRepositoryGetByIdTXDB(t *testing.T) {
+	dataSource := "root@tcp(localhost:3306)/storage"
+	txdb.Register("txdb_getbyid", "mysql", dataSource)
+	db, err := sql.Open("txdb_getbyid", uuid.New().String())
+	assert.NoError(t, err)
+
+	repo := NewRepository(db)
+	productId := 6
+	expectedProduct := domain.Product{
+		Id:        productId,
+		Nombre:    "Carro",
+		Color:     "Azul",
+		Precio:    300,
+		Stock:     6,
+		Codigo:    "C3456",
+		Publicado: true,
+		Fecha:     "23-04-12",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.GetById(ctx, productId)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, expectedProduct, p)
+}
+
+func TestRepositoryUpdate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	productId := 1
+	columns := []string{"id", "nombre", "color", "precio", "stock", "codigo", "publicado", "fecha"}
+	rows := sqlmock.NewRows(columns)
+	rows.AddRow(productId, "Avion", "Blanco", 788, 8, "AF456", true, "02-09-22")
+	mock.ExpectPrepare(regexp.QuoteMeta(Update))
+	mock.ExpectExec(regexp.QuoteMeta(Update)).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	repo := NewRepository(db)
+	updatedProduct := domain.Product{
+		Id:        productId,
+		Nombre:    "Avioneta",
+		Color:     "Rojo",
+		Precio:    800,
+		Stock:     10,
+		Codigo:    "Af41",
+		Publicado: true,
+		Fecha:     "05-06-20",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Update(ctx, updatedProduct.Id, updatedProduct.Nombre, updatedProduct.Color, updatedProduct.Precio, updatedProduct.Stock, updatedProduct.Codigo, updatedProduct.Publicado, updatedProduct.Fecha)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, updatedProduct, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestRepositoryUpdateTXDB(t *testing.T) {
+	dataSource := "root@tcp(localhost:3306)/storage"
+	txdb.Register("txdb_update", "mysql", dataSource)
+	db, err := sql.Open("txdb_update", uuid.New().String())
+	assert.NoError(t, err)
+
+	repo := NewRepository(db)
+	productId := 6
+	updatedProduct := domain.Product{
+		Id:        productId,
+		Nombre:    "Carro",
+		Color:     "Verde",
+		Precio:    300,
+		Stock:     6,
+		Codigo:    "C3456",
+		Publicado: true,
+		Fecha:     "23-04-12",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Update(ctx, updatedProduct.Id, updatedProduct.Nombre, updatedProduct.Color, updatedProduct.Precio, updatedProduct.Stock, updatedProduct.Codigo, updatedProduct.Publicado, updatedProduct.Fecha)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, updatedProduct, p)
+}
+
+func TestRepositoryDelete(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	productId := 1
+	columns := []string{"id", "nombre", "color", "precio", "stock", "codigo", "publicado", "fecha_creacion"}
+	rows := sqlmock.NewRows(columns)
+	rows.AddRow(productId, "Carro", "Verde", 300, 10, "C3456", true, "23-04-12")
+
+	mock.ExpectPrepare(regexp.QuoteMeta(Delete))
+	mock.ExpectQuery(regexp.QuoteMeta(GetById)).WillReturnRows(rows)
+	mock.ExpectExec(regexp.QuoteMeta(Delete)).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectQuery(regexp.QuoteMeta(GetById)).WillReturnRows(rows)
+	mock.ExpectQuery(GetAllProducts).WillReturnRows(rows)
+
+	repo := NewRepository(db)
+	deletedProduct := domain.Product{
+		Id:        productId,
+		Nombre:    "Carro",
+		Color:     "Verde",
+		Precio:    300,
+		Stock:     10,
+		Codigo:    "C3456",
+		Publicado: true,
+		Fecha:     "23-04-12",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Delete(ctx, deletedProduct.Id)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, deletedProduct, p)
+
+	p2, err2 := repo.GetById(ctx, deletedProduct.Id)
+	assert.NoError(t, err2)
+	assert.Zero(t, p2)
+
+	p3, err3 := repo.GetAll(ctx)
+	assert.NoError(t, err3)
+	assert.NotContains(t, p3, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+func TestRepositoryDeleteTXDB(t *testing.T) {
+	dataSource := "root@tcp(localhost:3306)/storage"
+	txdb.Register("txdb_delete", "mysql", dataSource)
+	db, err := sql.Open("txdb_delete", uuid.New().String())
+	assert.NoError(t, err)
+
+	repo := NewRepository(db)
+	deletedProduct := domain.Product{
+		Id:        6,
+		Nombre:    "Carro",
+		Color:     "Azul",
+		Precio:    300,
+		Stock:     6,
+		Codigo:    "C3456",
+		Publicado: true,
+		Fecha:     "23-04-12",
+	}
+
+	ctx := context.TODO()
+	p, err := repo.Delete(ctx, deletedProduct.Id)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.Equal(t, deletedProduct, p)
+
+	p2, err2 := repo.GetById(ctx, deletedProduct.Id)
+	assert.NoError(t, err2)
+	assert.Zero(t, p2)
+
+	p3, err3 := repo.GetAll(ctx)
+	assert.NoError(t, err3)
+	assert.NotContains(t, p3, p)
+}
+
+func TestRepositoryGetByIdFail(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	productId := 1
+	columns := []string{"id", "nombre", "color", "precio", "stock", "codigo", "publicado", "fecha_creacion"}
+	rows := sqlmock.NewRows(columns)
+
+	mock.ExpectQuery(regexp.QuoteMeta(GetById)).WillReturnRows(rows)
+
+	repo := NewRepository(db)
+
+	ctx := context.TODO()
+	p, err := repo.GetById(ctx, productId)
+	assert.NoError(t, err)
+	assert.Zero(t, p)
+	assert.Equal(t, domain.Product{}, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+func TestRepositoryGetByIdFailTXDB(t *testing.T) {
+	dataSource := "root@tcp(localhost:3306)/storage"
+	txdb.Register("txdb_getbyid_fail", "mysql", dataSource)
+	db, err := sql.Open("txdb_getbyid_fail", uuid.New().String())
+	assert.NoError(t, err)
+
+	repo := NewRepository(db)
+	productId := 1
+
+	ctx := context.TODO()
+	p, err := repo.GetById(ctx, productId)
+	assert.NoError(t, err)
+	assert.Zero(t, p)
+	assert.Equal(t, domain.Product{}, p)
 }
