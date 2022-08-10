b5646db0dde6903d02a4739ca5dfcd9595754b90
cristian39velez <cristian.velez@mercadolibre.com.co>
2022-08-10 09:51:36 -0500

    STORAGE_C2_TT

--- storage/proyecto_meli/go.mod
+++ storage/proyecto_meli/go.mod
@@ -12,6 +12,8 @@ require (
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
+	github.com/DATA-DOG/go-txdb v0.1.5 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
@@ -26,6 +28,7 @@ require (
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- storage/proyecto_meli/internal/products/repository_test.go
+++ storage/proyecto_meli/internal/products/repository_test.go
@@ -3,10 +3,15 @@ package products
 import (
 	"context"
 	"database/sql"
+	"proyecto_meli/internal/domain"
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/DATA-DOG/go-txdb"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetAll(t *testing.T) {
@@ -21,4 +26,108 @@ func TestGetAll(t *testing.T) {
 	if err != nil {
 		t.Errorf("err must be nil, but got %v", err)
 	}
+}
+
+func TestStorageTXDB(t *testing.T) {
+	//Arrange
+	txdb.Register("txdb", "mysql", "root@tcp(localhost:3306)/storage")
+	db, err := sql.Open("txdb", uuid.New().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	repo := NewRepository(db)
+	product := domain.Product{
+		Nombre:        "new product",
+		Color:         "white",
+		Precio:        12000,
+		Stock:         10,
+		Codigo:        "NEW01",
+		Publicado:     true,
+		FechaCreacion: "2022-01-01 00:00:00",
+	}
+	//Act
+	p, err := repo.Store(context.TODO(), product)
+	//Assert
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	//Act
+	p2, err := repo.GetById(context.TODO(), p.Id)
+	//Assert
+	assert.NoError(t, err)
+	assert.NotZero(t, p2)
+	assert.Equal(t, p, p2)
+}
+
+func TestUpdateTXDB(t *testing.T) {
+	//Arrabge
+	txdb.Register("txdb", "mysql", "root@tcp(localhost:3306)/storage")
+	db, err := sql.Open("txdb", uuid.New().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	repo := NewRepository(db)
+	product := domain.Product{
+		Nombre:        "new product",
+		Color:         "white",
+		Precio:        12000,
+		Stock:         10,
+		Codigo:        "NEW01",
+		Publicado:     true,
+		FechaCreacion: "2022-01-01 00:00:00",
+	}
+	//Act
+	p, err := repo.Store(context.TODO(), product)
+	//Assert
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	//Arrange
+	updateProduct := domain.Product{
+		Id:            p.Id,
+		Nombre:        "update product",
+		Color:         "red",
+		Precio:        120,
+		Stock:         1,
+		Codigo:        "UP01",
+		Publicado:     false,
+		FechaCreacion: "2022-01-02 00:00:00",
+	}
+	//Act
+	p2, err := repo.Update(context.TODO(), updateProduct)
+	assert.NoError(t, err)
+	assert.NotZero(t, p2)
+	assert.Equal(t, updateProduct, p2)
+	//Act
+	err = repo.Delete(context.TODO(), p.Id)
+	//Assert
+	assert.NoError(t, err)
+	//Act
+	p3, err := repo.GetById(context.TODO(), p.Id)
+	assert.Error(t, err)
+	assert.Zero(t, p3)
+}
+
+func TestStorageMock(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+	mock.ExpectPrepare("INSERT INTO products")
+	mock.ExpectExec("INSERT INTO products").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	productId := 1
+	repo := NewRepository(db)
+	product := domain.Product{
+		Id:            productId,
+		Nombre:        "new product",
+		Color:         "white",
+		Precio:        12000,
+		Stock:         10,
+		Codigo:        "NEW01",
+		Publicado:     true,
+		FechaCreacion: "2022-01-01 00:00:00",
+	}
+	//Act
+	p, err := repo.Store(context.TODO(), product)
+	assert.NoError(t, err)
+	assert.NotZero(t, p)
+	assert.NoError(t, mock.ExpectationsWereMet())
 }
